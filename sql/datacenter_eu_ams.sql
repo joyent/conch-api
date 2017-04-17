e3693f3452e068a10c2c8acdb7f69263293a9b5e
Bryan Horstmann-Allen <bdha@joyent.com>
2017-04-17 05:27:03 +0000

    Literally every time

--- sql/datacenter_eu_ams.sql
+++ sql/datacenter_eu_ams.sql
@@ -1,9 +1,9 @@
 INSERT INTO datacenter (vendor, vendor_name, region, location )
-       VALUES ("Equinix", "AM", "eu-central-1", "Amsterdam, Netherlands");
+       VALUES ('Equinix', 'AM', 'eu-central-1', 'Amsterdam, Netherlands');
 
 INSERT INTO datacenter_room (datacenter, az, alias, vendor_name)
-       VALUES ( ( SELECT id FROM datacenter WHERE region = 'eu-central-1' ), "eu-central-1a", "AZ1", "AM1");
+       VALUES ( ( SELECT id FROM datacenter WHERE region = 'eu-central-1' ), 'eu-central-1a', 'AZ1', 'AM1');
 INSERT INTO datacenter_room (datacenter, az, alias, vendor_name)
-       VALUES ( ( SELECT id FROM datacenter WHERE region = 'eu-central-1' ), "eu-central-1b", "AZ2", "AM3");
+       VALUES ( ( SELECT id FROM datacenter WHERE region = 'eu-central-1' ), 'eu-central-1b', 'AZ2', 'AM3');
 INSERT INTO datacenter_room (datacenter, az, alias, vendor_name)
-       VALUES ( ( SELECT id FROM datacenter WHERE region = 'eu-central-1' ), "eu-central-1c", "AZ3", "AM6");
+       VALUES ( ( SELECT id FROM datacenter WHERE region = 'eu-central-1' ), 'eu-central-1c', 'AZ3', 'AM6');
