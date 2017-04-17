2a41ec44610c6c6b09bba476f6acb5e45b704f52
Bryan Horstmann-Allen <bdha@joyent.com>
2017-04-17 05:11:31 +0000

    More AMS bits

--- sql/datacenter_eu_ams.sql
+++ sql/datacenter_eu_ams.sql
@@ -1,10 +1,9 @@
 INSERT INTO datacenter (vendor, vendor_name, region, location )
-       VALUES ("Equinix", "AM1", "eu-ams-1", "Amsterdam, Netherlands");
+       VALUES ("Equinix", "AM", "eu-central-1", "Amsterdam, Netherlands");
 
 INSERT INTO datacenter_room (datacenter, az, alias, vendor_name)
-       VALUES ( ( SELECT id FROM datacenter WHERE region = 'eu-ams-1' ), "eu-ams-1a", "AZ1", "AM1:01:310260");
+       VALUES ( ( SELECT id FROM datacenter WHERE region = 'eu-central-1' ), "eu-central-1a", "AZ1", "AM1");
 INSERT INTO datacenter_room (datacenter, az, alias, vendor_name)
-       VALUES ( ( SELECT id FROM datacenter WHERE region = 'eu-ams-1' ), "eu-ams-1b", "AZ2", "AM3:0G:010403");
+       VALUES ( ( SELECT id FROM datacenter WHERE region = 'eu-central-1' ), "eu-central-1b", "AZ2", "AM3");
 INSERT INTO datacenter_room (datacenter, az, alias, vendor_name)
-       VALUES ( ( SELECT id FROM datacenter WHERE region = 'eu-ams-1' ), "eu-ams-1c", "AZ3", "AM6:01:000Z3G");
-
+       VALUES ( ( SELECT id FROM datacenter WHERE region = 'eu-central-1' ), "eu-central-1c", "AZ3", "AM6");
