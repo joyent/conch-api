37c9dc4ebd8130ad8a262f3a7ba1e7a0378142ea
Bryan Horstmann-Allen <bdha@joyent.com>
2017-04-21 06:04:46 +0000

    Add CERES hw profile

--- sql/hardware_profiles.sql
+++ sql/hardware_profiles.sql
@@ -21,3 +21,9 @@ INSERT INTO hardware_product_profile ( product_id, purpose, bios_firmware, cpu_n
       VALUES (  ( SELECT id FROM hardware_product WHERE name = 'Joyent-Compute-Platform-3302' ),
             'SSD Compute', 'Dell Inc. 2.2.5', 2, 'Intel(R) Xeon(R) CPU E5-2690 v4 @ 2.60GHz',
             0, 16, 258372, 7, 16, 1490.42, '0-15', 2 );
+
+INSERT INTO hardware_product_profile ( product_id, purpose, bios_firmware, cpu_num, cpu_type,
+            dimms_num, ram_total, nics_num, sas_num, sas_size, ssd_num, ssd_size, ssd_slots, psu_total )
+      VALUES (  ( SELECT id FROM hardware_product WHERE name = 'CERES' ),
+            'CPG Compute', 'Dell Inc. 2.2.5', 2, 'Intel(R) Xeon(R) CPU E5-2690 v4 @ 2.60GHz',
+            16, 258372, 7, 15, 1117.81, 1, 93.16, '0', 2 );
