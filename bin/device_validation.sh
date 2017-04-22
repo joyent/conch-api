8175a862ca1623a85ad85a1525ce0d1eadff585e
Bryan Horstmann-Allen <bdha@joyent.com>
2017-04-22 04:00:03 +0000

    Naive update to device_validation report

--- bin/device_validation.sh
+++ bin/device_validation.sh
@@ -7,5 +7,10 @@ fi
 
 SN=$1
 
-echo "$SN: Marking graduated to production"
-psql preflight -U preflight -c "select device_id,report_id,component_type,component_name,metric,log,status,created from device_validate where device_id = '$SN' order by created"
+if [ "$2" ] ; then
+  STATUS="and status = $2"
+fi
+
+
+REPORT_ID=$( psql -q -t -A -d preflight -U preflight -c "select report_id from device_validate where device_id = '$SN' order by created desc limit 1" )
+psql -d preflight -U preflight -c "select device_id,report_id,component_type,component_name,metric,log,status,created from device_validate where report_id = '$REPORT_ID' $STATUS order by created desc"
