5f05b9884fcb29e7e08ee536d7cd5597aa702773
Bryan Horstmann-Allen <bdha@joyent.com>
2017-07-25 03:09:14 +0000

    Remove MANTA_META rack role, define in layouts instead

--- sql/rack_roles.sh
+++ sql/rack_roles.sh
@@ -7,7 +7,7 @@ PSQL="psql -d conch -U conch"
 
 ROLE_INSERT_SQL="INSERT INTO datacenter_rack_role ( name, rack_size )"
 
-for ROLE in TRITON MANTA MANTA_META CERES ; do
+for ROLE in TRITON MANTA CERES ; do
   $PSQL -c "$ROLE_INSERT_SQL VALUES ( '$ROLE', 45 )"
 done
 
