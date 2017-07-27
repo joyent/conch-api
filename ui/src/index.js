6ad84fe20081cf62683acbcd399b78b6d95d18bf
Jared Morrow <jm@joyent.com>
2017-07-27 14:39:10 -0600

    Holding until I get cors enabled on dancer

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,3 +1,59 @@
 var m = require("mithril");
+//var localStorage = require("localStorage");
 
-m.render(document.body, "hello world 2");
+var Auth = {
+    username: "",
+    password: "",
+
+    setUsername: function(value) {
+        Auth.username = value;
+    },
+    setPassword: function(value) {
+        Auth.password = value;
+    },
+    login: function() {
+        m.request({
+            method: "POST",
+            url: "http://10.64.223.75:5000/login",
+            data: {username: Auth.username, password: Auth.password},
+            config: (xhr) => {
+                xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+            }
+        }).then(function(data) {
+            console.log("logged in successfully and got " + data.token);
+            localStorage.setItem("auth-token", data.token);
+            m.route.set("/racks");
+        });
+    }
+}
+
+var Login = {
+    view: function() {
+        return m("form", [
+            m("input[type=text]", {
+                oninput: m.withAttr("value", Auth.setUsername),
+                value: Auth.username}),
+            m("input[type=password]", {
+                oninput: m.withAttr("value", Auth.setPassword),
+                value: Auth.password}),
+            m("button[type=button]", {
+                onclick: Auth.login},
+              "Login blah")]);
+    }
+}
+
+var RackList = require("./views/RackList");
+
+m.route(document.body, "/list", {
+    "/list": {
+        onmatch: function() {
+            if (!localStorage.getItem("auth-token")) {
+                return m.route.set("/login");
+            }
+            else {
+                return RackList;
+            }
+        }
+    },
+    "/login": Login
+});
