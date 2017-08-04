f59fcd46de3c218ad044a7cb4f75f59419c704d5
Lane Seppala <lane.seppala@joyent.com>
2017-08-04 08:22:14 -0600

    Assign devices to slots

--- Conch/ui/src/models/Rack.js
+++ Conch/ui/src/models/Rack.js
@@ -26,6 +26,26 @@ var Rack = {
         }).catch(function(e) {
             console.log("Error in GET /rack/" + id + ": " + e.message);
         });
+    },
+    assignDevices: function(rack) {
+        var deviceAssignments =
+            Object.keys(rack.slots).reduce(function(obj, slot) {
+                var device = rack.slots[slot].occupant;
+                if (device) {
+                    obj[device] = slot;
+                }
+                return obj;
+            }, {});
+        return m.request({
+            method: "POST",
+            url: "/rack/" + rack.id + "/layout",
+            data: deviceAssignments,
+            withCredentials: true
+        }).then(function(res) {
+            console.log(res);
+        }).catch(function(e) {
+            console.log("Error in assigning devices" + e.message);
+        });
     }
 }
 
--- Conch/ui/src/views/Rack.js
+++ Conch/ui/src/views/Rack.js
@@ -5,7 +5,6 @@ var allRacks = {
     oninit: Rack.loadRooms,
     view: function() {
         return m(".room-list", Object.keys(Rack.rackRooms).map(
-
           function(roomName) {
             return [
               m("h3.room-list-header", roomName),
@@ -24,35 +23,77 @@ var allRacks = {
     }
 };
 
+// Focus on the next Slot input when Enter is pressed.
+function enterAsTab(e) {
+    try {
+        var nextInput = e.target.parentNode.parentNode
+                .nextSibling.lastChild.lastChild;
+        if (e.which == 13 && nextInput) {
+                nextInput.focus();
+                e.preventDefault();
+            }
+    }
+    catch (e){ }
+}
+
+var rackLayoutTable = { view: function () {
+    return m("table.pure-table.pure-table-horizontal.pure-table-striped", [
+        m("thead", m("tr", [
+            m("th", "Slot number"),
+            m("th", "Name"),
+            m("th", "Alias"),
+            m("th", "Vendor"),
+            m("th", "Size"),
+            m("th", "Device")
+        ])),
+        m("tbody",
+            Object.keys(Rack.current.slots || {}).reverse().map(function(slot) {
+                return m("tr", [
+                    m("td", slot),
+                    m("td", Rack.current.slots[slot].name),
+                    m("td", Rack.current.slots[slot].alias),
+                    m("td", Rack.current.slots[slot].vendor),
+                    m("td", Rack.current.slots[slot].size),
+                    m("td",
+                        m("input[type=text][placeholder=Unassigned]",
+                            { oninput:
+                                m.withAttr("value",
+                                    function(value) {
+                                        Rack.current.slots[slot].occupant = value;
+                                    }
+                                ),
+                                id: "slot-" + slot,
+                                onkeypress: enterAsTab,
+                                value: Rack.current.slots[slot].occupant
+                            }
+                        )
+                    )
+                ]);
+            }))
+    ]);
+} };
 
 var rackLayout = {
     oninit: function(vnode) { Rack.load(vnode.attrs.id) },
     view: function() {
-        console.log(Rack.current.slots);
-        return [
-            m("h1", Rack.current.datacenter),
-            m("h2", Rack.current.name),
-            m("h2", Rack.current.role),
-            m("table.pure-table.pure-table-horizontal.pure-table-striped", [
-                m("thead", m("tr", [
-                    m("th", "Slot number"),
-                    m("th", "Name"),
-                    m("th", "Alias"),
-                    m("th", "Vendor"),
-                    m("th", "Size")
-                ])),
-                m("tbody",
-                    Object.keys(Rack.current.slots || {}).map(function(slot) {
-                        return m("tr", [
-                            m("td", slot),
-                            m("td", Rack.current.slots[slot].name),
-                            m("td", Rack.current.slots[slot].alias),
-                            m("td", Rack.current.slots[slot].vendor),
-                            m("td", Rack.current.slots[slot].size),
-                        ]);
-                    }))
-            ])
-        ];
+        return m(".pure-g", [
+            m(".pure-u-1-3", m("h3", "Datacenter")),
+            m(".pure-u-1-3", m("h3", "Rack Name")),
+            m(".pure-u-1-3", m("h3", "Rack Role")),
+            m(".pure-u-1-3", Rack.current.datacenter),
+            m(".pure-u-1-3", Rack.current.name),
+            m(".pure-u-1-3", Rack.current.role),
+            m(".pure-u-1", 
+                m("form.pure-form",
+                    { onsubmit: function (e) {
+                        e.preventDefault; Rack.assignDevices(Rack.current);
+                    } },
+                [
+                    m("button.pure-button.pure-button-primary[type=submit]", "Assign Devices"),
+                    m(rackLayoutTable),
+                ])
+            )
+        ]);
     }
 };
 
