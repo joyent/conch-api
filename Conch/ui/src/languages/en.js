f6a1b0c694d64ff2051cd2ca84b361de83c59582
Lane Seppala <lane.seppala@joyent.com>
2017-08-08 16:57:40 -0600

    Translate main menu

---
+++ Conch/ui/src/languages/en.js
@@ -0,0 +1,15 @@
+module.exports = {
+    'values': {
+
+        // Main menu items
+        'Racks' : 'Racks',
+        'Problems' : 'Problems',
+        'Devices' : 'Devices',
+
+        // Login items
+        'Login to Conch' : 'Login to Conch',
+        'User Name': 'User Name',
+        'Password' : 'Password',
+        'Login' : 'Login'
+    }
+};
