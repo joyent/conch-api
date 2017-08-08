fd5065c421e7918edc9a0592d91053048fd04f3e
Lane Seppala <lane.seppala@joyent.com>
2017-08-08 17:24:58 -0600

    Localize Racks page

--- Conch/ui/src/languages/en.js
+++ Conch/ui/src/languages/en.js
@@ -1,15 +1,34 @@
 module.exports = {
-    'values': {
+    "values": {
 
         // Main menu items
-        'Racks' : 'Racks',
-        'Problems' : 'Problems',
-        'Devices' : 'Devices',
+        "Racks"    : "Racks",
+        "Problems" : "Problems",
+        "Devices"  : "Devices",
 
         // Login items
-        'Login to Conch' : 'Login to Conch',
-        'User Name': 'User Name',
-        'Password' : 'Password',
-        'Login' : 'Login'
+        "Login to Conch" : "Login to Conch",
+        "User Name"      : "User Name",
+        "Password"       : "Password",
+        "Login"          : "Login",
+
+        // Rack page
+        "Name"           : "Name",
+        "Rack Name"      : "Rack Name",
+        "Rack Role"      : "Rack Role",
+        "Role"           : "Role",
+        "Datacenter"     : "Datacenter",
+        "Size"           : "Size",
+        "Slot Number"    : "Slot #",
+        "Alias"          : "Alias",
+        "Vendor"         : "Vendor",
+        "Device"         : "Device",
+        "Unassigned"     : "Unassigned",
+        "Select Rack"    : "Select a rack in the sidebar",
+        "Assign Devices" : "Assign Devices",
+        "Assign Success" : "Successfully assigned devices to rack",
+
+        // Problems page
+        "Select Device" : "Select a Device in the sidebar"
     }
 };
