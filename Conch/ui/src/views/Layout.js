d3a615493b3e1ab5beb916bf37c90b6a57437c1a
Lane Seppala <lane.seppala@joyent.com>
2017-08-11 10:38:49 -0600

    Add some styling to the sidebar

--- Conch/ui/src/views/Layout.js
+++ Conch/ui/src/views/Layout.js
@@ -6,19 +6,24 @@ module.exports = {
         return m("main.layout", [
             m(".pure-g", [
               m(".pure-u-1-12.pure-menu.nav",
+                m("h1", t("Conch")),
                 m("ul.pure-menu-list",[
-                  m("li.pure-menu-item.nav-item",
-                    m("a[href='/rack'].pure-menu-link",
+
+                  m("li.pure-menu-item",
+                    m("a[href='/rack'].pure-menu-link.nav-link",
                       {oncreate: m.route.link}, t("Racks"))
                   ),
-                  m("li.pure-menu-item.nav-item",
-                    m("a[href='/problem'].pure-menu-link",
+
+                  m("li.pure-menu-item",
+                    m("a[href='/problem'].pure-menu-link.nav-link",
                       {oncreate: m.route.link}, t("Problems"))
                   ),
-                  m("li.pure-menu-item.nav-item",
-                    m("a[href='/device'].pure-menu-link",
+
+                  m("li.pure-menu-item",
+                    m("a[href='/device'].pure-menu-link.nav-link",
                       {oncreate: m.route.link}, t("Devices"))
                   ),
+
                 ]
                 )),
                 vnode.children
