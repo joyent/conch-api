16809b250202eae7262a89347a81377a0c7ba8a5
Lane Seppala <lane.seppala@joyent.com>
2017-08-18 11:58:06 -0600

    Add more information and organization to Problem page

--- Conch/ui/src/models/Problem.js
+++ Conch/ui/src/models/Problem.js
@@ -1,5 +1,13 @@
 var m = require("mithril");
 
+function sortObject(obj) {
+    return Object.keys(obj).sort().reduce(
+        function(acc, i) {
+            acc[i] = obj[i];
+            return acc;
+        }, {});
+}
+
 var Problem = {
     devices: {},
     loadDeviceProblems: function() {
@@ -8,12 +16,11 @@ var Problem = {
             url: "/problem",
             withCredentials: true
         }).then(function(res) {
-            Problem.devices =
-                Object.keys(res).sort().reduce(
-                    function(acc, i) {
-                        acc[i] = res[i];
-                        return acc;
-                    }, {});
+            Problem.devices = {
+                failing    : sortObject(res.failing),
+                unlocated  : sortObject(res.unlocated),
+                unreported : sortObject(res.unreported),
+            };
         }).catch(function(e) {
             if (e.error === "unauthorized") {
                 m.route.set("/login");
@@ -23,15 +30,22 @@ var Problem = {
             }
         });
     },
+    deviceHasProblem: function(deviceId) {
+        // Search through all categories for a matching deviceId
+        return Object.keys(Problem.devices).some(function(group) {
+            return Problem.devices[group][deviceId];
+        });
+    },
     selected: null,
     selectDevice: function (deviceId) {
-        return m.request({
-            method: "GET",
-            url: "/problem",
-            withCredentials: true
-        }).then(function(res) {
-            Problem.selected = res[deviceId];
-        });
+        return Problem.loadDeviceProblems()
+            .then(function() {
+                Object.keys(Problem.devices).forEach(function(group) {
+                    if (Problem.devices[group][deviceId]) {
+                        Problem.selected = Problem.devices[group][deviceId];
+                    }
+                });
+            });
     }
 };
 
