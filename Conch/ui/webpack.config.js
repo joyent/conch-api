e4d2ec6f7f202299bd7cf9b9b0e426eab1a1eac2
Lane Seppala <lane.seppala@joyent.com>
2017-09-07 11:27:19 -0600

    Enable ES6 in UI, rework `make watch`

---
+++ Conch/ui/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, './bin'),
+        filename: 'app.js',
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }]
+    }
+}
