568f9cf089fe4679654f418a37b5342e4d7246d0
Lane Seppala <lane.seppala@joyent.com>
2017-09-07 16:54:42 -0600

    Add Relay page

--- Conch/ui/src/index.js
+++ Conch/ui/src/index.js
@@ -1,15 +1,17 @@
 var m = require("mithril");
 var t = require('i18n4v');
 
-var Device  = require("./views/Device");
-var Layout  = require("./views/Layout");
-var Login   = require("./views/Login");
-var Problem = require("./views/Problem");
-var Rack    = require("./views/Rack");
-var Status = require("./views/Status");
+var Device      = require("./views/Device");
+var Layout      = require("./views/Layout");
+var Login       = require("./views/Login");
+var Problem     = require("./views/Problem");
+var Rack        = require("./views/Rack");
+var Status      = require("./views/Status");
+var RelayList   = require("./views/Relay/List");
+var RelayDetail = require("./views/Relay/Detail");
 
 var korean = require('./languages/ko.js');
-var languages = {
+const languages = {
     en: require('./languages/en.js'),
     ko: korean,
     'ko-KR': korean
@@ -80,6 +82,22 @@ m.route(document.body, "/", {
             return m(Layout.threePane, { active : 2, title: "Report"  },
                 m(Device.allDevices),
                 m(Device.deviceReport, vnode.attrs)
+            );
+        }
+    },
+    "/relay" : {
+        render : (vnode) => {
+            return m(Layout.threePane, { active : 1, title: "Relays"  },
+                m(RelayList),
+                m(RelayDetail)
+            );
+        }
+    },
+    "/relay/:id" : {
+        render : ({attrs}) => {
+            return m(Layout.threePane, { active : 2, title: "Relay"  },
+                m(RelayList, attrs),
+                m(RelayDetail, attrs)
             );
         }
     },
---
+++ Conch/ui/src/views/Relay/Detail.js
@@ -0,0 +1,128 @@
+const m = require('mithril');
+const t = require('i18n4v');
+const moment = require('moment');
+
+const Relay  = require('../../models/Relay');
+const Device = require('../../models/Device');
+
+const DeviceStatus = require('../component/DeviceStatus');
+const Table = require('../component/Table');
+
+module.exports = {
+    loading : false,
+    oninit : () => {
+        Relay.loadCurrentRelay();
+    },
+
+    view : ({state}) => {
+        if (state.loading)
+            return m(".loading", "Loading...");
+        if (!Relay.current)
+            return m(".make-selection", t("select a relay"));
+        if (state.loading)
+            return m(".loading", t("Loading"));
+        const relayInfo =
+            Table(Relay.current.id,
+                [
+                    t("IP Address"),
+                    t("SSH Port Tunnel"),
+                    t("Version"),
+                    t("Last Seen"),
+                ],
+                [[
+                    Relay.current.ipaddr || t("No IP Address"),
+                    Relay.current.ssh_port,
+                    Relay.current.version,
+                    moment(Relay.current.updated).fromNow()
+                ]]
+            );
+        const locationInfo = Relay.current.location ?
+            Table(t("Relay Location"),
+                [
+                    t("Datacenter Room"),
+                    t("Rack Name"),
+                    t("Role Name"),
+                    t("Actions")
+                ],
+                [[
+                    Relay.current.location.room_name,
+                    m("a", {
+                            href : "/rack/" + Relay.current.location.rack_id,
+                            oncreate : m.route.link
+                        },Relay.current.location.rack_name),
+                    Relay.current.location.role_name,
+                    m("a.pure-button",
+                        {
+                            href : "/rack/" + Relay.current.location.rack_id,
+                            oncreate : m.route.link,
+                            title : t("Show Rack")
+                        },
+                        m("i.material-icons.md-18", "dns")
+                    ),
+                ]]
+            )
+            : null ;
+        const deviceActions = device => {
+            return [
+                m("button.pure-button",
+                    {
+                        onclick : () => {
+                            state.loading = true;
+                            Device.getDeviceLocation(device.id)
+                                .then(loc =>
+                                    m.route.set(`/rack/${loc.rack.id}?device=${device.id}`)
+                                );
+                        },
+                        title : t("Find Device in Rack")
+                    },
+                    m("i.material-icons.md-18", "dns")
+                ),
+                device.health === 'PASS' ?
+                    m("a.pure-button",
+                        {
+                            href : "/problem/" + device.id,
+                            oncreate : m.route.link,
+                            title : t("Show Device Problems")
+                        },
+                        m("i.material-icons.md-18", "report_problem")
+                    )
+                    : null,
+                m("a.pure-button",
+                    {
+                        href : "/device/" + device.id,
+                        oncreate : m.route.link,
+                        title : t("Latest Device Report")
+                    },
+                    m("i.material-icons.md-18", "description")
+                ),
+            ]
+        }
+        const deviceTable =
+            Table(t("Connected Devices"),
+                [
+                    t("Status"),
+                    t("Device"),
+                    t("Last Seen"),
+                    t("Actions")
+                ],
+                Relay.current.devices.map(device => {
+                    return [
+                        m(DeviceStatus, { device : device }),
+                        device.id,
+                        m("span.time",
+                            { title: device.last_seen },
+                            moment(device.last_seen).fromNow()
+                        ),
+                        deviceActions(device)
+                    ];
+                })
+            );
+        return [
+            relayInfo,
+            locationInfo,
+            deviceTable
+        ];
+    }
+
+};
+
