e6080560327d7392764a59827b0474e3a94caac7
Lane Seppala <lane.seppala@joyent.com>
2017-09-07 18:50:18 -0600

    Add DeviceStatus component

---
+++ Conch/ui/src/views/component/DeviceStatus.js
@@ -0,0 +1,30 @@
+const m = require('mithril');
+
+const Icons = require("./Icons");
+const Device = require("../../models/Device");
+
+// Given a Device, renders icons in a <div> showing the current status.
+module.exports = {
+    view : ({attrs}) => {
+        const device = attrs.device;
+        if (device) {
+
+            let healthIcon;
+            if (device.health === 'PASS')
+                healthIcon = Icons.passValidation;
+            else if (device.health === 'FAIL')
+                healthIcon = Icons.failValidation;
+            else
+                healthIcon = Icons.noReport;
+
+            return m(".device-status",
+                [
+                    healthIcon,
+                    Device.isActive(device) ?
+                    Icons.deviceReporting
+                    : null,
+                ]);
+        }
+        return m(".device-status");
+    }
+};
