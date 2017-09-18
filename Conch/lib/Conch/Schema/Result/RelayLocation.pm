06daaaa054ec5d84a3b1867352cb468f1c36c2c7
Lane Seppala <lane.seppala@joyent.com>
2017-09-18 16:09:41 -0600

    Make Relay location exclusive

--- Conch/lib/Conch/Schema/Result/RelayLocation.pm
+++ Conch/lib/Conch/Schema/Result/RelayLocation.pm
@@ -10,6 +10,8 @@ __PACKAGE__->table('RelayLocation');
 #
 # Has the same columns as 'Device'
 __PACKAGE__->add_columns(
+  "relay_id",
+  { data_type => "text", is_nullable => 0 },
   "rack_id",
   { data_type => "uuid", is_nullable => 0, size => 16 },
   "rack_name",
@@ -24,23 +26,33 @@ __PACKAGE__->add_columns(
 # do not attempt to deploy() this view
 __PACKAGE__->result_source_instance->is_virtual(1);
 
-# Takes a relay ID and returns the list of devices connected to that relay
+# For all relays, give the *exclusive* rack location based on the most recent
+# timestamp for any devices connected to the rack.
+#
+# Example: PRD1 connects to device A in rack 1. PRD1 is removed and PRD2
+# connects to Device B which also happens to be in rack 1. This query should
+# show that PRD2 is connected to rack 1 and PRD 1 is not.
+#
 __PACKAGE__->result_source_instance->view_definition(q[
-  SELECT rack.id as rack_id, rack.name as rack_name,
+  SELECT drc1.relay_id, rack.id as rack_id, rack.name as rack_name,
     room.az as room_name, role.name as role_name
-  FROM device_location loc
+  FROM device_relay_connection drc1
+  JOIN device d ON
+    drc1.device_id = d.id
+  JOIN device_location loc ON
+    loc.device_id = d.id
   JOIN datacenter_rack rack
     ON loc.rack_id = rack.id
   JOIN datacenter_room room
     ON rack.datacenter_room_id = room.id
   JOIN datacenter_rack_role role
     ON rack.role = role.id
-  WHERE loc.device_id IN (
-    SELECT device_id
-    FROM device_relay_connection
-    WHERE relay_id = ?
-    ORDER BY last_seen desc
-    LIMIT 1
+  WHERE drc1.last_seen = (
+    SELECT max(drc2.last_seen)
+    FROM device_relay_connection drc2
+    JOIN device_location loc2
+      ON drc2.device_id = loc2.device_id
+    WHERE loc.rack_id = loc2.rack_id
   )
 ]);
 
