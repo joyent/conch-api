2e4d37fe1a1eea2312c61b7126a7463684f092ef
Lane Seppala <lane.seppala@joyent.com>
2017-10-19 14:21:01 -0600

    Add reset-database to sql/ and Makefile

--- sql/load.sh
+++ sql/load.sh
@@ -3,16 +3,18 @@
 set -e
 set -x
 
+BASEDIR=$(cd `dirname $0` && pwd)
+
 psql -U postgres -d conch -c 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";'
 psql -U postgres -d conch -c 'CREATE EXTENSION IF NOT EXISTS "pgcrypto";'
 
 PSQL="psql -A -U conch -d conch"
 
 for F in conch.sql hardware.sql zpool_profiles.sql hardware_profiles.sql validate_criteria.sql ; do
-  $PSQL < $F
+  $PSQL < $BASEDIR/$F
 done
 
-./rack_roles.sh
+$BASEDIR/rack_roles.sh
 
 # Run migrations
-./run_migrations.sh
+$BASEDIR/run_migrations.sh
---
+++ sql/reset-database.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+BASEDIR=$(cd `dirname $0` && pwd)
+
+psql -d postgres -c 'DROP DATABASE conch'
+psql -d postgres -c 'DROP DATABASE conch'
+psql -d postgres -c 'CREATE ROLE conch'
+psql -d postgres -c 'CREATE DATABASE conch OWNER conch'
+
+$BASEDIR/load.sh
+
