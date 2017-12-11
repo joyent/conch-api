ede008cbb50132203d787b242a270e12e04cab7f
Bryan Horstmann-Allen <bdha@joyent.com>
2017-12-11 16:58:39 +0000

    Make new column a text field

--- sql/migrations/0017-add-enclosures.sql
+++ sql/migrations/0017-add-enclosures.sql
@@ -2,7 +2,7 @@ SELECT run_migration(17, $$
 
     -- Add a new enclosure column and copy the (incorrectly defined as) hba
     -- data into it.
-    ALTER TABLE device_disk ADD COLUMN enclosure INTEGER;
+    ALTER TABLE device_disk ADD COLUMN enclosure text;
     UPDATE device_disk SET enclosure = hba;
 
 $$);
