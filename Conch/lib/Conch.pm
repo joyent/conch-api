da20a459c0b43f1fc462fc1134cde886779e6811
Lane Seppala <lane.seppala@joyent.com>
2017-12-12 12:45:02 -0700

    feat(api): Add GET /hardware_product and GET /hardware_product/:id
    Phil needs access to hardare product information to complete the build
    out of northeast-1a. This adds two endpoints to API:
    
    1. list all hardware products defined **that has an associated hardware
    product profile** (context:
    https://chat.joyent.us/joyent/pl/1m6osos1s3gctgc4ts65x518ra)
    2. get a single hardware product by ID. It also must have hardware
    product profile.
    
    Each of these include details from the hardware product profile and
    zpool profile (if defined) in the response. All responses specified in
    OpenAPI spec.

--- Conch/lib/Conch.pm
+++ Conch/lib/Conch.pm
@@ -2,10 +2,11 @@ package Conch;
 use Dancer2;
 use Conch::Route::User;
 use Conch::Route::Device;
+use Conch::Route::Feedback;
+use Conch::Route::HardwareProduct;
+use Conch::Route::Problem;
 use Conch::Route::Rack;
 use Conch::Route::Relay;
-use Conch::Route::Problem;
-use Conch::Route::Feedback;
 use Conch::Route::Workspace;
 
 our $VERSION = '2.0';
