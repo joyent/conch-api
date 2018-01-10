3c65d0558493aaf3e1af2d610b6c79005bd1e4a3
Dale Ghent <daleg@joyent.com>
2018-01-09 19:12:25 -0500

    Make usb drive count configurable

---
+++ sql/migrations/0019-add-usb_num.sql
@@ -0,0 +1,13 @@
+SELECT run_migration(19, $$
+
+    -- Add a new usb_num column.
+    ALTER TABLE hardware_product_profile ADD COLUMN usb_num integer;
+
+    -- Set all profiles to require 1 usb disk
+    UPDATE hardware_product_profile SET usb_num = 1;
+
+    -- Set JCP-3211 to require 0 usb disks
+    UPDATE hardware_product_profile SET usb_num = 0 WHERE product_id =
+    (SELECT id FROM hardware_product WHERE name='Joyent-Compute-Platform-3211');
+
+$$);
