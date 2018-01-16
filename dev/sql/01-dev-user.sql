0f51c318e2469ca85a47aeaefb697d7bab2c2cb3
sungo <sungo@joyent.com>
2018-01-16 11:49:03 -0500

    build: add a (non-functional) email address to the conch dev user

--- dev/sql/01-dev-user.sql
+++ dev/sql/01-dev-user.sql
@@ -1,5 +1,5 @@
 begin;
-insert into user_account(name, password_hash) values('conch','{CRYPT}$2a$04$h963P26i4rTMaVogvA2U7ePcZTYm2o0gfSHyxaUCZSuthkpg47Zbi');
+insert into user_account(name, email, password_hash) values('conch','conch@conch.joyent.us','{CRYPT}$2a$04$h963P26i4rTMaVogvA2U7ePcZTYm2o0gfSHyxaUCZSuthkpg47Zbi');
 insert into user_workspace_role(user_id,workspace_id,role_id) values(
 	(select id from user_account where name='conch' limit 1),
 	(select id from workspace where name='GLOBAL' limit 1),
