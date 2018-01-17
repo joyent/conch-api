3daf6c9e45d46c51be1d60bd8d1b2d099180c434
sungo <sungo@joyent.com>
2018-01-17 17:47:49 -0500

    refactor: we never actually use as_v1_json for DatacenterRack so we dont need a special case variant

--- Conch/lib/Conch/Class/DatacenterRack.pm
+++ Conch/lib/Conch/Class/DatacenterRack.pm
@@ -6,14 +6,5 @@ with 'Conch::Class::Role::JsonV1';
 
 has [qw( id name role_name datacenter_room_id )];
 
-sub as_v1_json {
-  my $self = shift;
-  {
-    id        => $self->id,
-    name      => $self->name,
-    role_name => $self->role_name
-  };
-}
-
 1;
 
