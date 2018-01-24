1799d43fc745f3d43ac61a8f9711723937b2e469
Lane Seppala <lane.seppala@joyent.com>
2018-01-23 18:07:03 -0700

    chore: format DeviceReport controller

--- Conch/lib/Conch/Controller/DeviceReport.pm
+++ Conch/lib/Conch/Controller/DeviceReport.pm
@@ -12,65 +12,64 @@ use Conch::Legacy::Data::Report::Switch;
 use Conch::Legacy::Data::Report::Server;
 
 # TODO: None of the available Mojolicious Log4Perl libraries allow selecting
-# the category (appender) for Log4Perl. We use this mechanism to log unparsable
-# device reports and device reports that result in processing exceptions
+# the category (appender) for Log4Perl. We used this mechanism to log unparsable
+# device reports and device reports that result in processing exceptions.
 sub process ($c) {
-  my $raw_report = $c->req->json;
+	my $raw_report = $c->req->json;
 
-  #Log::Any->get_logger( category => 'report.raw' )
-  #->trace( encode_json $raw_report);
-  my $device_report = $c->_parse_device_report($raw_report);
+	#Log::Any->get_logger( category => 'report.raw' )
+	#->trace( encode_json $raw_report);
+	my $device_report = $c->_parse_device_report($raw_report);
 
-  if ( $device_report->is_fail ) {
+	if ( $device_report->is_fail ) {
 
-    #my $err_log = Log::Any->get_logger( category => 'report.unparsable' );
-    $c->app->log->error(
-      'Failed parsing device report: ' . $device_report->failure );
-    return $c->status( 400, { error => $device_report->failure } );
-  }
+		#my $err_log = Log::Any->get_logger( category => 'report.unparsable' );
+		$c->app->log->error(
+			'Failed parsing device report: ' . $device_report->failure );
+		return $c->status( 400, { error => $device_report->failure } );
+	}
 
-  # Use the old device report recording and device validation code for now.
-  # This will be removed when OPS-RFD 22 is implemented
-  my $schema = Conch::Legacy::Schema->connect( $c->pg->dsn, $c->pg->username,
-    $c->pg->password );
+	# Use the old device report recording and device validation code for now.
+	# This will be removed when OPS-RFD 22 is implemented
+	my $schema = Conch::Legacy::Schema->connect( $c->pg->dsn, $c->pg->username,
+		$c->pg->password );
 
-  my ( $device, $report_id ) =
-    record_device_report( $schema, $device_report->value );
-  my $validation_result =
-    validate_device( $schema, $device, $device_report->value, $report_id );
+	my ( $device, $report_id ) =
+		record_device_report( $schema, $device_report->value );
+	my $validation_result =
+		validate_device( $schema, $device, $device_report->value, $report_id );
 
-  $c->status( 200, $validation_result );
+	$c->status( 200, $validation_result );
 }
 
 # Parse a report object from a HashRef and report all validation errors
 # Returns a list where the first element may be the parsed log and the second
 # may be validation errors, but not both.
 sub _parse_device_report ( $self, $input ) {
-  my $aux_report = dclone($input);
+	my $aux_report = dclone($input);
 
-  my $report;
-  if ( $input->{device_type} && $input->{device_type} eq "switch" ) {
-    $report = try { Conch::Legacy::Data::Report::Switch->new($input) };
-  }
-  else {
-    $report = try { Conch::Legacy::Data::Report::Server->new($input) };
-  }
+	my $report;
+	if ( $input->{device_type} && $input->{device_type} eq "switch" ) {
+		$report = try { Conch::Legacy::Data::Report::Switch->new($input) };
+	}
+	else {
+		$report = try { Conch::Legacy::Data::Report::Server->new($input) };
+	}
 
-  if ( $report->is_fail ) {
-    my $errs = join( "; ", map { $_->message } $report->failure->errors );
-    $self->log->warn("Error validating device report: $errs");
-    return fail($errs);
-  }
-  else {
-    for my $attr ( keys %{ $report->value->pack } ) {
-      delete $aux_report->{$attr};
-    }
-    if ( %{$aux_report} ) {
-      $report->value->{aux} = $aux_report;
-    }
-    return $report;
-  }
+	if ( $report->is_fail ) {
+		my $errs = join( "; ", map { $_->message } $report->failure->errors );
+		$self->log->warn("Error validating device report: $errs");
+		return fail($errs);
+	}
+	else {
+		for my $attr ( keys %{ $report->value->pack } ) {
+			delete $aux_report->{$attr};
+		}
+		if ( %{$aux_report} ) {
+			$report->value->{aux} = $aux_report;
+		}
+		return $report;
+	}
 }
 
 1;
-
