1325e779cd3f9c90fa1a4044abd91fa13904a0ce
sungo <sungo@joyent.com>
2018-01-31 15:32:40 -0500

    feat: in Workspace, restrict the rights of "Integrator" users

--- Conch/lib/Conch/Controller/Workspace.pm
+++ Conch/lib/Conch/Controller/Workspace.pm
@@ -96,6 +96,7 @@ sub create_sub_workspace ($c) {
 	my $ws             = $c->stash('current_workspace');
 
 	return $c->status(401) if $ws->role eq 'Read-only';
+	return $c->status(401) if $ws->role eq 'Integrator';
 
 	my $sub_ws_attempt = $c->workspace->create_sub_workspace(
 		$c->stash('user_id'), $ws->id, $ws->role_id,
