c085f099e26770fd476a45dc6de68c19847b8413
sungo <sungo@joyent.com>
2018-03-01 18:02:03 -0500

    feat: new Conch::Models class to let a user pull in all the models in one go

---
+++ Conch/lib/Conch/Models.pm
@@ -0,0 +1,43 @@
+=pod
+
+=head1 NAME
+
+Conch::Models
+
+=head1 DESCRIPTION
+
+Convenience class to load all the Conch::Model classes
+
+=cut
+
+package Conch::Models;
+
+use Conch::Model::DeviceLocation;
+use Conch::Model::DeviceReport;
+use Conch::Model::DeviceSettings;
+use Conch::Model::HardwareProduct;
+use Conch::Model::Relay;
+use Conch::Model::Workspace;
+use Conch::Model::WorkspaceDevice;
+use Conch::Model::WorkspaceRack;
+use Conch::Model::WorkspaceRelay;
+use Conch::Model::WorkspaceRoom;
+use Conch::Model::WorkspaceUser;
+use Conch::Model::WorkspaceRole;
+
+1;
+
+__DATA__
+
+=pod
+
+=head1 LICENSING
+
+Copyright Joyent, Inc.
+
+This Source Code Form is subject to the terms of the Mozilla Public License, 
+v.2.0. If a copy of the MPL was not distributed with this file, You can obtain
+one at http://mozilla.org/MPL/2.0/.
+
+=cut
+
