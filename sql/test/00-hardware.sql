510192c5f6e64708af9b9d633641cc9bab80ed7f
Bryan Horstmann-Allen <bdha@joyent.com>
2018-03-02 13:01:10 +1100

    Cute, but ;)

--- sql/test/00-hardware.sql
+++ sql/test/00-hardware.sql
@@ -1,11 +1,11 @@
-INSERT INTO hardware_vendor (name) VALUES ('DellHell');
+INSERT INTO hardware_vendor (name) VALUES ('DellBell');
 INSERT INTO hardware_vendor (name) VALUES ('SuperDuperMicro');
 
 INSERT INTO hardware_product (name, alias, prefix, vendor)
-       VALUES ( 'Switch', 'Farce 10', 'F10', ( SELECT id FROM hardware_vendor WHERE name = 'DellHell' ) );
+       VALUES ( 'Switch', 'Farce 10', 'F10', ( SELECT id FROM hardware_vendor WHERE name = 'DellBell' ) );
 
 INSERT INTO hardware_product (name, alias, prefix, vendor)
-       VALUES ( 'Joyent-Compute-Platform', 'Test Compute', 'HA', ( SELECT id FROM hardware_vendor WHERE name = 'DellHell' ) );
+       VALUES ( 'Joyent-Compute-Platform', 'Test Compute', 'HA', ( SELECT id FROM hardware_vendor WHERE name = 'DellBell' ) );
 
 INSERT INTO hardware_product (name, alias, prefix, vendor)
        VALUES ( 'Joyent-Storage-Platform', 'Test Storage', 'MS', ( SELECT id FROM hardware_vendor WHERE name = 'SuperDuperMicro' ) );
