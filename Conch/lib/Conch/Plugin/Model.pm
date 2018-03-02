f6de961d997ed6fbaf96a39bfa75648f274e18e8
sungo <sungo@joyent.com>
2018-03-02 11:36:56 -0500

    refactor: remove workspace_relay model helper

--- Conch/lib/Conch/Plugin/Model.pm
+++ Conch/lib/Conch/Plugin/Model.pm
@@ -33,12 +33,6 @@ Sets up Mojo helpers for all the models
 
 sub register ( $self, $app, $conf ) {
 	$app->helper(
-		workspace_relay => sub {
-			state $workspace_relay =
-				Conch::Model::WorkspaceRelay->new();
-		}
-	);
-	$app->helper(
 		workspace_room => sub {
 			state $workspace_room =
 				Conch::Model::WorkspaceRoom->new();
