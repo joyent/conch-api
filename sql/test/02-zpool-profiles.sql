4806fda918209428d0b0c61e02653a1c3c893145
Dale Ghent <daleg@joyent.com>
2018-07-09 22:02:01 +0000

    more test updates

--- sql/test/02-zpool-profiles.sql
+++ sql/test/02-zpool-profiles.sql
@@ -1,9 +1,9 @@
 
 INSERT INTO zpool_profile (name, vdev_t, vdev_n, disk_per, spare, log, cache)
-       VALUES ( 'Joyent-Compute-Platform', 'mirror', 7, 2, 1, 1, 0 );
+       VALUES ( '2-ssds-1-cpu', 'mirror', 7, 2, 1, 1, 0 );
 
 INSERT INTO zpool_profile (name, vdev_t, vdev_n, disk_per, spare, log, cache)
-       VALUES ( 'Joyent-Storage-Platform', 'raidz2', 3, 11, 2, 1, 0 );
+       VALUES ( '65-ssds-2-cpu', 'raidz2', 3, 11, 2, 1, 0 );
 
 UPDATE hardware_product_profile
     SET zpool_id =
