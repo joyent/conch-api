544e2930cb643374fe270aab783b75906329c7eb
sungo <sungo@joyent.com>
2018-07-10 11:33:15 -0400

    refactor: whitespace pedantry

--- Conch/lib/Conch/Model/HardwareProduct.pm
+++ Conch/lib/Conch/Model/HardwareProduct.pm
@@ -19,46 +19,46 @@ use Conch::Pg;
 # Query fields used to build HardwareProduct, which depends on
 # HardwareProductProfile and ZpoolProfile
 my $fields = q{
-  hw_product.id AS hw_product_id,
-  hw_product.name AS hw_product_name,
-  hw_product.alias AS hw_product_alias,
-  hw_product.prefix AS hw_product_prefix,
-  hw_product.specification as hw_specification,
-  hw_product.sku as hw_sku,
-  hw_product.generation_name as hw_generation_name,
-  hw_product.legacy_product_name as hw_legacy_product_name,
-  vendor.name AS hw_product_vendor,
+	  hw_product.id AS hw_product_id,
+	  hw_product.name AS hw_product_name,
+	  hw_product.alias AS hw_product_alias,
+	  hw_product.prefix AS hw_product_prefix,
+	  hw_product.specification as hw_specification,
+	  hw_product.sku as hw_sku,
+	  hw_product.generation_name as hw_generation_name,
+	  hw_product.legacy_product_name as hw_legacy_product_name,
+	  vendor.name AS hw_product_vendor,
 
-  hw_profile.id AS hw_profile_id,
-  hw_profile.bios_firmware AS hw_profile_bios_firmware,
-  hw_profile.cpu_num AS hw_profile_cpu_num,
-  hw_profile.cpu_type AS hw_profile_cpu_type,
-  hw_profile.dimms_num AS hw_profile_dimms_num,
-  hw_profile.hba_firmware AS hw_profile_hba_firmware,
-  hw_profile.nics_num AS hw_profile_nics_num,
-  hw_profile.psu_total AS hw_profile_psu_total,
-  hw_profile.purpose AS hw_profile_purpose,
-  hw_profile.rack_unit AS hw_profile_rack_unit,
-  hw_profile.ram_total AS hw_profile_ram_total,
-  hw_profile.sas_num AS hw_profile_sas_num,
-  hw_profile.sas_size AS hw_profile_sas_size,
-  hw_profile.sas_slots AS hw_profile_sas_slots,
-  hw_profile.sata_num AS hw_profile_sata_num,
-  hw_profile.sata_size AS hw_profile_sata_size,
-  hw_profile.sata_slots AS hw_profile_sata_slots,
-  hw_profile.ssd_num AS hw_profile_ssd_num,
-  hw_profile.ssd_size AS hw_profile_ssd_size,
-  hw_profile.ssd_slots AS hw_profile_ssd_slots,
-  hw_profile.usb_num AS hw_profile_usb_num,
+	  hw_profile.id AS hw_profile_id,
+	  hw_profile.bios_firmware AS hw_profile_bios_firmware,
+	  hw_profile.cpu_num AS hw_profile_cpu_num,
+	  hw_profile.cpu_type AS hw_profile_cpu_type,
+	  hw_profile.dimms_num AS hw_profile_dimms_num,
+	  hw_profile.hba_firmware AS hw_profile_hba_firmware,
+	  hw_profile.nics_num AS hw_profile_nics_num,
+	  hw_profile.psu_total AS hw_profile_psu_total,
+	  hw_profile.purpose AS hw_profile_purpose,
+	  hw_profile.rack_unit AS hw_profile_rack_unit,
+	  hw_profile.ram_total AS hw_profile_ram_total,
+	  hw_profile.sas_num AS hw_profile_sas_num,
+	  hw_profile.sas_size AS hw_profile_sas_size,
+	  hw_profile.sas_slots AS hw_profile_sas_slots,
+	  hw_profile.sata_num AS hw_profile_sata_num,
+	  hw_profile.sata_size AS hw_profile_sata_size,
+	  hw_profile.sata_slots AS hw_profile_sata_slots,
+	  hw_profile.ssd_num AS hw_profile_ssd_num,
+	  hw_profile.ssd_size AS hw_profile_ssd_size,
+	  hw_profile.ssd_slots AS hw_profile_ssd_slots,
+	  hw_profile.usb_num AS hw_profile_usb_num,
 
-  zpool.id AS zpool_id,
-  zpool.name AS zpool_name,
-  zpool.cache AS zpool_cache,
-  zpool.log AS zpool_log,
-  zpool.disk_per AS zpool_disk_per,
-  zpool.spare AS zpool_spare,
-  zpool.vdev_n AS zpool_vdev_n,
-  zpool.vdev_t AS zpool_vdev_t
+	  zpool.id AS zpool_id,
+	  zpool.name AS zpool_name,
+	  zpool.cache AS zpool_cache,
+	  zpool.log AS zpool_log,
+	  zpool.disk_per AS zpool_disk_per,
+	  zpool.spare AS zpool_spare,
+	  zpool.vdev_n AS zpool_vdev_n,
+	  zpool.vdev_t AS zpool_vdev_t
 };
 
 =head2 list
@@ -68,19 +68,17 @@ profiles.
 
 =cut
 sub list ($self) {
-	my $hw_product_hashes = Conch::Pg->new->db->query(
-		qq{
-      SELECT $fields
-      FROM hardware_product hw_product
-      JOIN hardware_product_profile hw_profile
-        ON hw_product.id = hw_profile.product_id
-      JOIN hardware_vendor vendor
-        ON hw_product.vendor = vendor.id
-      LEFT JOIN zpool_profile zpool
-        ON hw_profile.zpool_id = zpool.id
-      WHERE hw_product.deactivated IS NULL
-    }
-	)->hashes->to_array;
+	my $hw_product_hashes = Conch::Pg->new->db->query(qq{
+		  SELECT $fields
+		  FROM hardware_product hw_product
+		  JOIN hardware_product_profile hw_profile
+			ON hw_product.id = hw_profile.product_id
+		  JOIN hardware_vendor vendor
+			ON hw_product.vendor = vendor.id
+		  LEFT JOIN zpool_profile zpool
+			ON hw_profile.zpool_id = zpool.id
+		  WHERE hw_product.deactivated IS NULL
+	})->hashes->to_array;
 	return [ map { _build_hardware_product($_) } @$hw_product_hashes ];
 }
 
@@ -91,20 +89,18 @@ profile by ID.
 
 =cut
 sub lookup ( $self, $hw_id ) {
-	my $ret = Conch::Pg->new->db->query(
-		qq{
-        SELECT $fields
-        FROM hardware_product hw_product
-        JOIN hardware_product_profile hw_profile
-          ON hw_product.id = hw_profile.product_id
-        JOIN hardware_vendor vendor
-          ON hw_product.vendor = vendor.id
-        LEFT JOIN zpool_profile zpool
-          ON hw_profile.zpool_id = zpool.id
-        WHERE hw_product.deactivated IS NULL
-          AND hw_product.id = ?
-      }, $hw_id
-	)->hash;
+	my $ret = Conch::Pg->new->db->query(qq{
+		SELECT $fields
+		FROM hardware_product hw_product
+		JOIN hardware_product_profile hw_profile
+		  ON hw_product.id = hw_profile.product_id
+		JOIN hardware_vendor vendor
+		  ON hw_product.vendor = vendor.id
+		LEFT JOIN zpool_profile zpool
+		  ON hw_profile.zpool_id = zpool.id
+		WHERE hw_product.deactivated IS NULL
+		  AND hw_product.id = ?
+      }, $hw_id)->hash;
 	return undef unless $ret;
 	return _build_hardware_product($ret);
 }
@@ -116,20 +112,18 @@ profile by the hardware product name.
 
 =cut
 sub lookup_by_name ( $self, $name ) {
-	my $ret = Conch::Pg->new->db->query(
-		qq{
-        SELECT $fields
-        FROM hardware_product hw_product
-        JOIN hardware_product_profile hw_profile
-          ON hw_product.id = hw_profile.product_id
-        JOIN hardware_vendor vendor
-          ON hw_product.vendor = vendor.id
-        LEFT JOIN zpool_profile zpool
-          ON hw_profile.zpool_id = zpool.id
-        WHERE hw_product.deactivated IS NULL
-          AND hw_product.name = ?
-      }, $name
-	)->hash;
+	my $ret = Conch::Pg->new->db->query(qq{
+		SELECT $fields
+		FROM hardware_product hw_product
+		JOIN hardware_product_profile hw_profile
+		  ON hw_product.id = hw_profile.product_id
+		JOIN hardware_vendor vendor
+		  ON hw_product.vendor = vendor.id
+		LEFT JOIN zpool_profile zpool
+		  ON hw_profile.zpool_id = zpool.id
+		WHERE hw_product.deactivated IS NULL
+		  AND hw_product.name = ?
+	}, $name)->hash;
 	return undef unless $ret;
 
 	return _build_hardware_product($ret);
@@ -142,23 +136,21 @@ profile by the hardware SKU.
 
 =cut
 sub lookup_by_sku ( $self, $sku ) {
-	my $ret = Conch::Pg->new->db->query(
-		qq{
-        SELECT $fields
-        FROM hardware_product hw_product
-        JOIN hardware_product_profile hw_profile
-          ON hw_product.id = hw_profile.product_id
-        JOIN hardware_vendor vendor
-          ON hw_product.vendor = vendor.id
-        LEFT JOIN zpool_profile zpool
-          ON hw_profile.zpool_id = zpool.id
-        WHERE hw_product.deactivated IS NULL
-          AND hw_product.sku = ?
-      }, $sku
-	)->hash;
-return undef unless $ret;
+	my $ret = Conch::Pg->new->db->query(qq{
+		SELECT $fields
+		FROM hardware_product hw_product
+		JOIN hardware_product_profile hw_profile
+		  ON hw_product.id = hw_profile.product_id
+		JOIN hardware_vendor vendor
+		  ON hw_product.vendor = vendor.id
+		LEFT JOIN zpool_profile zpool
+		  ON hw_profile.zpool_id = zpool.id
+		WHERE hw_product.deactivated IS NULL
+		  AND hw_product.sku = ?
+      }, $sku)->hash;
+	return undef unless $ret;
 
-return _build_hardware_product($ret);
+	return _build_hardware_product($ret);
 }
 
 sub _build_hardware_product ($hw) {
