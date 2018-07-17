b9f8b08f6aebac149005d6a8935be57d16994f9d
sungo <sungo@joyent.com>
2018-07-17 11:41:46 -0400

    when handling an exception, use the exception that is always in the stash but not always in the args

--- Conch/lib/Conch.pm
+++ Conch/lib/Conch.pm
@@ -147,7 +147,7 @@ sub startup {
 			my ( $c, $args ) = @_;
 			return unless my $template = $args->{template};
 			if ( $template =~ /exception/ ) {
-				my $exception = $args->{exception};
+				my $exception = $c->stash('exception');
 				$exception->verbose(1);
 				$self->log->error($exception);
 
