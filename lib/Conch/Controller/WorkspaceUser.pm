joyent/conch-api

commit bb2c8e26cce50b3e5eff6a039005fe45357a1836

lots more logging (also some light reformatting)

lib/Conch/Controller/WorkspaceUser.pm
@@ -10,11 +10,14 @@ Conch::Controller::WorkspaceUser
 
 package Conch::Controller::WorkspaceUser;
 
+use Role::Tiny::With;
 use Mojo::Base 'Mojolicious::Controller', -signatures;
 use Data::Printer;
 
 use Conch::Models;
 use Conch::Pg;
+
+with 'Conch::Role::MojoLog';
 
 =head2 list
 
@@ -24,7 +27,10 @@ Get a list of users for the current stashed C<current_workspace>
 
 sub list ($c) {
 	my $users = Conch::Model::WorkspaceUser->new->workspace_users(
-		$c->stash('current_workspace')->id );
+		$c->stash('current_workspace')->id
+	);
+
+	$c->log->debug("Found ".scalar($users->@*)." users");
 	$c->status( 200, $users );
 }
 
@@ -38,19 +44,27 @@ sub invite ($c) {
 	my $body = $c->req->json;
 	return $c->status(403) unless $c->is_admin;
 
-	return $c->status( 400, { error => '"user" and "role " fields required ' } )
-		unless ( $body->{user} and $body->{role} );
+	unless($body->{user} and $body->{role}) {
+		# FIXME actually use the validator
+		$c->log->warn("Input failed validation");
+		return $c->status( 400, { 
+			error => '"user" and "role " fields required'
+		});
+	}
 
-	my $ws = $c->stash('current_workspace');
-	my $maybe_role =
-		Conch::Model::WorkspaceRole->new->lookup_by_name( $body->{role} );
+	my $maybe_role = Conch::Model::WorkspaceRole->new->lookup_by_name(
+		$body->{role}
+	);
 
 	unless ($maybe_role) {
-		my $role_names =
-			join( ', ',
-			map { $_->name } @{ Conch::Model::WorkspaceRole->new->list } );
-		return $c->status( 400,
-			{ error => '"role" must be one of: ' . $role_names } );
+		my $role_names = join( ', ',
+			map { $_->name } @{ Conch::Model::WorkspaceRole->new->list }
+		);
+
+		$c->log->debug("Role name '".$body->{role}."' was not one of $role_names");
+		return $c->status( 400 => {
+				error => '"role" must be one of: ' . $role_names 
+		});
 	}
 
 	# TODO: it would be nice to be sure of which type of data we were being passed here, so we
@@ -59,18 +73,31 @@ sub invite ($c) {
 		|| $c->db_user_accounts->lookup_by_name($body->{user});
 
 	unless ($user) {
+		$c->log->debug("User '".$body->{user}."' was not found");
+
 		my $password = $c->random_string();
 		$user = $c->db_user_accounts->create({
-			email => $body->{user},
-			name => $body->{user},	# FIXME: we should always have a name.
-			password => $password,	# will be hashed in constructor
+			email    => $body->{user},
+			name     => $body->{user}, # FIXME: we should always have a name.
+			password => $password,     # will be hashed in constructor
 		});
-		$c->mail->send_new_user_invite(
-			{ email => $user->email, password => $password } );
+
+		$c->log->info("User '".$body->{user}."' was created with ID ".$user->id);
+		$c->mail->send_new_user_invite({
+			email    => $user->email,
+			password => $password
+		});
+
+		# TODO update this complain when we stop sending plaintext passwords
+		$c->log->warn("Email sent to ".$user->email." containing their PLAINTEXT password");
 	}
 
-	Conch::Model::Workspace->new->add_user_to_workspace( $user->id, $ws->id,
-		$maybe_role->id );
+	Conch::Model::Workspace->new->add_user_to_workspace(
+		$user->id,
+		$c->stash('current_workspace'),
+		$maybe_role->id
+	);
+	$c->log->info("Add user ".$user->id." to workspace ".$c->stash('current_workspace')->id);
 	$c->status(201);
 }
 

lib/Conch/Controller/DB/HardwareProduct.pm
@@ -1,6 +1,9 @@
 package Conch::Controller::DB::HardwareProduct;
 
+use Role::Tiny::With;
 use Mojo::Base 'Mojolicious::Controller', -signatures;
+
+with 'Conch::Role::MojoLog';
 
 use Conch::Models;
 
@@ -16,6 +19,9 @@ sub under ($c) {
 	if($c->param('id') =~ /^(.+?)\=(.+)$/) {
 		my $k = $1;
 		my $v = $2;
+
+		$c->log->debug("Looking up a HardwareProduct by identifier ($k = $v)");
+
 		if($k eq "sku") {
 			$h = $c->schema->resultset("HardwareProduct")->single({
 				sku => $v,
@@ -32,10 +38,12 @@ sub under ($c) {
 				deactivated => undef,
 			});
 		} else {
+			$c->log->debug("Unknown identifier '$k' passed to HardwareProduct lookup");
 			$c->status('501');
 			return undef;
 		}
 	} else {
+		$c->log->debug("Looking up a HardwareProduct by id ".$c->param('id'));
 		$h = $c->schema->resultset("HardwareProduct")->single({
 			id => $c->param('id'),
 			deactivated => undef,
@@ -43,9 +51,11 @@ sub under ($c) {
 	}
 
 	if ($h) {
+		$c->log->debug("Found hardware product ".$h->id);
 		$c->stash('hardware_product' => $h);
 		return 1;
 	} else {
+		$c->log->debug("Could not locate a valid hardware product");
 		$c->status(404 => { error => "Not found" });
 		return undef;
 	}
@@ -61,6 +71,7 @@ sub get_all ($c) {
 	my @r = $c->schema->resultset("HardwareProduct")->search({
 		deactivated => undef,
 	})->all;
+	$c->log->debug("Found ".(scalar @r)." hardware products");
 	return $c->status(200, \@r);
 }
 
@@ -70,8 +81,7 @@ sub get_all ($c) {
 =cut
 
 sub get_one ($c) {
-	$c->status(200 => $c->stash('hardware_product')
-	);
+	$c->status(200 => $c->stash('hardware_product'));
 }
 
 
@@ -82,7 +92,10 @@ sub get_one ($c) {
 sub create ($c) {
 	return $c->status(403) unless $c->is_global_admin;
 	my $i = $c->validate_input('DBHardwareProductCreate');
-	return if not $i;
+	if(not $i) {
+		$c->log->warn("Input failed validation");
+		return;
+	}
 
 	for my $k (qw[name alias sku]) {
 		next unless $i->{$k};
@@ -90,6 +103,7 @@ sub create ($c) {
 			$k => $i->{$k}
 		})->all;
 		if (scalar @r > 0) {
+			$c->log->debug("Failed to create hardware product: unique constraint violation for $k");
 			return $c->status(400 => {
 				error => "Unique constraint violated on '$k'"
 			});
@@ -97,6 +111,8 @@ sub create ($c) {
 	}
 
 	my $r = $c->schema->resultset("HardwareProduct")->create($i);
+
+	$c->log->debug("Created hardware product ".$r->id);
 	$c->status(303 => "/db/hardware_product/".$r->id);
 }
 
@@ -114,10 +130,13 @@ sub update ($c) {
 	for my $k (qw[name alias sku]) {
 		next unless $i->{$k};
 		next if $i->{$k} eq $c->stash('hardware_product')->get_column($k);
+
 		my @r = $c->schema->resultset("HardwareProduct")->search({
 			$k => $i->{$k}
 		})->all;
+
 		if (scalar @r > 0) {
+			$c->log->debug("Failed to create hardware product: unique constraint violation for $k");
 			return $c->status(400 => {
 				error => "Unique constraint violated on '$k'"
 			});
@@ -126,6 +145,7 @@ sub update ($c) {
 
 
 	$c->stash('hardware_product')->update($i);
+	$c->log->debug("Updated hardware product ".$c->stash('hardware_product')->id);
 	$c->status(303 => "/db/hardware_product/".$c->stash('hardware_product')->id);
 }
 
@@ -137,6 +157,7 @@ sub update ($c) {
 sub delete ($c) {
 	return $c->status(403) unless $c->is_global_admin;
 	$c->stash('hardware_product')->update({ deactivated => 'NOW()' });
+	$c->log->debug("Deleted hardware product ".$c->stash('hardware_product')->id);
 	return $c->status(204);
 }
 

lib/Conch/Controller/Datacenter.pm
@@ -1,6 +1,9 @@
 package Conch::Controller::Datacenter;
 
+use Role::Tiny::With;
 use Mojo::Base 'Mojolicious::Controller', -signatures;
+
+with 'Conch::Role::MojoLog';
 
 use Conch::Models;
 
@@ -26,9 +29,11 @@ sub under ($c) {
 	}
 
 	if ($s) {
+		$c->log->debug("Found datacenter ".$c->param('id'));
 		$c->stash('datacenter' => $s);
 		return 1;
 	} else {
+		$c->log->debug("Unable to find datacenter ".$c->param('id'));
 		$c->status(404 => { error => "Not found" });
 		return undef;
 	}
@@ -44,7 +49,11 @@ Get all datacenters
 
 sub get_all ($c) {
 	return $c->status(403) unless $c->is_global_admin;
-	return $c->status(200, Conch::Model::Datacenter->all());
+	my @dc = Conch::Model::Datacenter->all()->@*;
+
+	$c->log->debug("Found ".scalar(@dc)." datacenters");
+
+	return $c->status(200, \@dc);
 }
 
 
@@ -69,9 +78,12 @@ Get all rooms for the given datacenter
 
 sub get_rooms ($c) {
 	return $c->status(403) unless $c->is_global_admin;
-	$c->status(200, Conch::Model::DatacenterRoom->from_datacenter(
+	my @r = Conch::Model::DatacenterRoom->from_datacenter(
 		$c->stash('datacenter')->id
-	));
+	)->@*;
+
+	$c->log->debug("Found ".scalar(@r)." datacenter rooms");
+	$c->status(200, \@r);
 }
 
 =head2 create
@@ -83,9 +95,13 @@ Create a new datacenter
 sub create ($c) {
 	return $c->status(403) unless $c->is_global_admin;
 	my $i = $c->validate_input('DatacenterCreate');
-	return if not $i;
+	if(not $i) {
+		$c->log->debug("Input failed validation");
+		return;
+	}	
 
 	my $r = Conch::Model::Datacenter->new($i->%*)->save;
+	$c->log->debug("Created datacenter ".$r->id);
 	$c->status(303 => "/dc/".$r->id);
 }
 
@@ -99,9 +115,13 @@ Update an existing datacenter
 sub update ($c) {
 	return $c->status(403) unless $c->is_global_admin;
 	my $i = $c->validate_input('DatacenterUpdate');
-	return if not $i;
+	if(not $i) {
+		$c->log->debug("Input failed validation");
+		return;
+	}
 
 	$c->stash('datacenter')->update($i->%*)->save();
+	$c->log->debug("Updated datacenter ".$c->stash('datacenter')->id);
 	$c->status(303 => "/dc/".$c->stash('datacenter')->id);
 }
 
@@ -115,6 +135,7 @@ Permanently delete a datacenter
 sub delete ($c) {
 	return $c->status(403) unless $c->is_global_admin;
 	$c->stash('datacenter')->burn;
+	$c->log->debug("Deleted datacenter ".$c->stash('datacenter')->id);
 	return $c->status(204);
 }
 

lib/Conch/Controller/DatacenterRack.pm
@@ -10,10 +10,13 @@ Conch::Controller::DatacenterRack
 
 package Conch::Controller::DatacenterRack;
 
+use Role::Tiny::With;
 use Mojo::Base 'Mojolicious::Controller', -signatures;
 use Conch::UUID 'is_uuid';
 
 use Conch::Models;
+
+with 'Conch::Role::MojoLog';
 
 
 =head2 under
@@ -28,24 +31,31 @@ sub under ($c) {
 		return undef;
 	}
 
-
 	my $r;
 
 	if($c->param('id') =~ /^(.+?)\=(.+)$/) {
-		if($1 eq 'name') {
-			$r = Conch::Model::DatacenterRack->from_name($2);
+		my $k = $1;
+		my $v = $2;
+
+		if($k eq 'name') {
+			$c->log->debug("Looking up a datacenter rack by identifier $k");
+			$r = Conch::Model::DatacenterRack->from_name($v);
 		} else {
+			$c->log->warn("Unsupported identifier '$k' found");
 			$c->status(404 => { error => "Not found" });
 			return undef;
 		}
 	} else {
+		$c->log->debug("Looking for datacenter rack ".$c->param('id'));
 		$r = Conch::Model::DatacenterRack->from_id($c->param('id'));
 	}
 
 	if ($r) {
+		$c->log->debug("Found datacenter rack ".$r->id);
 		$c->stash('rack' => $r);
 		return 1;
 	} else {
+		$c->log->debug("Could not find datacenter rack"); 
 		$c->status(404 => { error => "Not found" });
 		return undef;
 	}
@@ -58,7 +68,10 @@ sub under ($c) {
 sub create ($c) {
 	return $c->status(403) unless $c->is_global_admin;
 	my $d = $c->validate_input('RackCreate');
-	return if not $d;
+	if(not $d) {
+		$c->log->debug("Input failed validation");
+		return;
+	}
 
 	unless(Conch::Model::DatacenterRoom->from_id($d->{datacenter_room_id})) {
 		return $c->status(400 => { "error" => "Room does not exist" });
@@ -69,6 +82,7 @@ sub create ($c) {
 	}
 		
 	my $r = Conch::Model::DatacenterRack->new($d->%*)->save();
+	$c->log->debug("Created datacenter rack ".$r->id);
 
 	$c->status(303 => "/rack/".$r->id);
 
@@ -94,7 +108,11 @@ Get all racks
 
 sub get_all ($c) {
 	return $c->status(403) unless $c->is_global_admin;
-	$c->status(200, Conch::Model::DatacenterRack->all());
+
+	my $r = Conch::Model::DatacenterRack->all();
+	$c->log->debug("Found ".scalar($r->@*)." datacenter racks");
+
+	$c->status(200, $r);
 }
 
 =head2 layouts
@@ -103,10 +121,12 @@ sub get_all ($c) {
 
 sub layouts ($c) {
 	return $c->status(403) unless $c->is_global_admin;
-	$c->status(
-		200 =>
-		Conch::Model::DatacenterRackLayout->from_rack_id($c->stash('rack')->id)
+	my $l = Conch::Model::DatacenterRackLayout->from_rack_id(
+		$c->stash('rack')->id
 	);
+
+	$c->log->debug("Found ".scalar($l->@*)." datacenter rack layouts");
+	$c->status(200 => $l);
 }
 
 
@@ -117,7 +137,10 @@ sub layouts ($c) {
 
 sub update ($c) {
 	my $i = $c->validate_input('RackUpdate');
-	return if not $i;
+	if(not $i) {
+		$c->log->debug("Input failed validation");
+		return;
+	}
 
 	if($i->{datacenter_room_id}) {
 		unless(Conch::Model::DatacenterRoom->from_id($i->{datacenter_room_id})) {
@@ -132,6 +155,7 @@ sub update ($c) {
 	}
 
 	$c->stash('rack')->update($i->%*)->save();
+	$c->log->debug("Updated datacenter rack ".$c->stash('rack')->id);
 	return $c->status(303 => "/rack/".$c->stash('rack')->id);
 }
 
@@ -144,6 +168,7 @@ Delete a rack
 
 sub delete ($c) {
 	$c->stash('rack')->burn;
+	$c->log->debug("Deleted datacenter rack ".$c->stash('rack')->id);
 	return $c->status(204);
 }
 

lib/Conch/Controller/DatacenterRackLayout.pm
@@ -10,11 +10,14 @@ Conch::Controller::DatacenterRackLayout
 
 package Conch::Controller::DatacenterRackLayout;
 
+use Role::Tiny::With;
 use Mojo::Base 'Mojolicious::Controller', -signatures;
 use Conch::UUID 'is_uuid';
 
 use Conch::Models;
 use List::MoreUtils qw(firstval);
+
+with 'Conch::Role::MojoLog';
 
 
 =head2 under
@@ -32,9 +35,11 @@ sub under ($c) {
 	my $r = Conch::Model::DatacenterRackLayout->from_id($c->param('id'));
 
 	if ($r) {
+		$c->log->debug("Found datacenter rack layout ".$r->id);
 		$c->stash('rack_layout' => $r);
 		return 1;
 	} else {
+		$c->log->debug("Could not find datacenter rack layout ".$c->param('id'));
 		$c->status(404 => { error => "Not found" });
 		return undef;
 	}
@@ -47,25 +52,32 @@ sub under ($c) {
 sub create ($c) {
 	return $c->status(403) unless $c->is_global_admin;
 	my $d = $c->validate_input('RackLayoutCreate');
-	return if not $d;
+	if(not $d) {
+		$c->log->debug("Input failed validation");
+		return;
+	}
 
 	unless(Conch::Model::DatacenterRack->from_id($d->{rack_id})) {
+		$c->log->debug("Could not find datacenter rack ".$d->{rack_id});
 		return $c->status(400 => { "error" => "Rack does not exist" });
 	}
 
 	unless(Conch::Model::HardwareProduct->lookup($d->{product_id})) {
+		$c->log->debug("Could not find hardware product ".$d->{product_id});
 		return $c->status(400 => { "error" => "Hardware product does not exist" });
 	}
 
 	my @r = Conch::Model::DatacenterRackLayout->from_rack_id($d->{rack_id})->@*;
 	my $v = firstval { $_->{ru_start} == $d->{ru_start} } @r;
 	if($v) {
+		$c->log->debug("Conflict with ru_start value of ".$d->{ru_start});
 		return $c->status(400 => {
 			error => "ru_start conflict"
 		});
 	}
 
 	my $r = Conch::Model::DatacenterRackLayout->new($d->%*)->save();
+	$c->log->debug("Created datacenter rack layout ".$r->id);
 
 	$c->status(303 => "/layout/".$r->id);
 
@@ -88,7 +100,9 @@ sub get ($c) {
 
 sub get_all ($c) {
 	return $c->status(403) unless $c->is_global_admin;
-	$c->status(200, Conch::Model::DatacenterRackLayout->all());
+	my $d = Conch::Model::DatacenterRackLayout->all();
+	$c->log->debug("Found ".scalar($d->@*)." datacenter rack layouts");
+	$c->status(200 => $d);
 }
 
 =head2 update
@@ -119,6 +133,7 @@ sub update ($c) {
 		)->@*;
 
 		if($v) {
+			$c->log->debug("Conflict with ru_start value of ".$i->{ru_start});
 			return $c->status(400 => {
 				error => "ru_start conflict"
 			});
@@ -137,6 +152,7 @@ sub update ($c) {
 
 sub delete ($c) {
 	$c->stash('rack_layout')->burn;
+	$c->log->debug("Deleted datacenter rack layout ".$c->stash('rack_layout')->id);
 	return $c->status(204);
 }
 

lib/Conch/Controller/DatacenterRackRole.pm
@@ -10,10 +10,13 @@ Conch::Controller::DatacenterRackRole
 
 package Conch::Controller::DatacenterRackRole;
 
+use Role::Tiny::With;
 use Mojo::Base 'Mojolicious::Controller', -signatures;
 use Conch::UUID 'is_uuid';
 
 use Conch::Models;
+
+with 'Conch::Role::MojoLog';
 
 
 =head2 under
@@ -31,20 +34,26 @@ sub under ($c) {
 	my $r;
 
 	if($c->param('id') =~ /^(.+?)\=(.+)$/) {
-		if($1 eq 'name') {
-			$r = Conch::Model::DatacenterRackRole->from_name($2);
+		my ($k, $v) = ($1, $2);
+		if($k eq 'name') {
+			$c->log->debug("Looking up datacenter rack role using identifier '$k'");
+			$r = Conch::Model::DatacenterRackRole->from_name($v);
 		} else {
+			$c->log->warn("Unknown identifier '$k'");
 			$c->status(404 => { error => "Not found" });
 			return undef;
 		}
 	} else {
+		$c->log->debug("looking up datacenter rack role by id");
 		$r = Conch::Model::DatacenterRackRole->from_id($c->param('id'));
 	}
 
 	if ($r) {
+		$c->log->debug("Found datacenter rack role ".$r->id);
 		$c->stash('rack_role' => $r);
 		return 1;
 	} else {
+		$c->log->debug("Failed to find datacenter rack role");
 		$c->status(404 => { error => "Not found" });
 		return undef;
 	}
@@ -57,16 +66,19 @@ sub under ($c) {
 sub create ($c) {
 	return $c->status(403) unless $c->is_global_admin;
 	my $d = $c->validate_input('RackRoleCreate');
-	return if not $d;
+	if(not $d) {
+		$c->log->warn("Input failed validation");
+		return;
+	}
 
 	if(Conch::Model::DatacenterRackRole->from_name($d->{name})) {
+		$c->log->debug("Name conflict on '".$d->{name}."'");
 		return $c->status(400 => { error => "name is already taken" });
 	}
 
 	my $r = Conch::Model::DatacenterRackRole->new($d->%*)->save();
-	$c->status(303);
-	$c->redirect_to($c->url_for("/rack_role/".$r->id));
-
+	$c->log->debug("Created datacenter rack role ".$r->id);
+	$c->status(303 => "/rack_role/".$r->id);
 }
 
 =head2 get
@@ -90,7 +102,11 @@ Get all rack roles
 
 sub get_all ($c) {
 	return $c->status(403) unless $c->is_global_admin;
-	$c->status(200, Conch::Model::DatacenterRackRole->all());
+
+	my $r = Conch::Model::DatacenterRackRole->all();
+	$c->log->debug("Found ".scalar($r->@*)." datacenter rack roles");
+
+	$c->status(200 => $r);
 }
 
 
@@ -101,14 +117,19 @@ sub get_all ($c) {
 
 sub update ($c) {
 	my $i = $c->validate_input('RackRoleUpdate');
-	return if not $i;
+	if(not $i) {
+		$c->log->warn("Input failed validation");
+		return;
+	}
 
 	if ($i->{name}) {
 		if(Conch::Model::DatacenterRackRole->from_name($i->{name})) {
+			$c->log->debug("Name conflict on '".$i->{name}."'");
 			return $c->status(400 => { error => "name is already taken" });
 		}
 	}
 	$c->stash('rack_role')->update($i->%*)->save();
+	$c->log->debug("Updated datacenter rack role ".$c->stash('rack_role')->id);
 	$c->status(303 => "/rack_role/".$c->stash('rack_role')->id);
 }
 
@@ -121,6 +142,7 @@ Delete a rack role
 
 sub delete ($c) {
 	$c->stash('rack_role')->burn;
+	$c->log->debug("Deleted datacenter rack role ".$c->stash('rack_role')->id);
 	return $c->status(204);
 }
 

lib/Conch/Controller/DatacenterRoom.pm
@@ -1,8 +1,11 @@
 package Conch::Controller::DatacenterRoom;
 
+use Role::Tiny::With;
 use Mojo::Base 'Mojolicious::Controller', -signatures;
 
 use Conch::Models;
+
+with 'Conch::Role::MojoLog';
 
 
 =head2 under
@@ -20,16 +23,20 @@ sub under ($c) {
 	my $s;
 
 	if($c->param('id') =~ /^(.+?)\=(.+)$/) {
+		$c->log->warn("Unsupported identifier '$1'");
 		$c->status(501);
 		return undef;
 	} else {
+		$c->log->debug("Looking up datacenter room ".$c->param('id'));
 		$s = Conch::Model::DatacenterRoom->from_id($c->param('id'));
 	}
 
 	if ($s) {
+		$c->log->debug("Found datacenter room");
 		$c->stash('datacenter_room' => $s);
 		return 1;
 	} else {
+		$c->log->debug("Could not find datacenter room");
 		$c->status(404 => { error => "Not found" });
 		return undef;
 	}
@@ -45,7 +52,11 @@ Get all datacenter rooms
 
 sub get_all ($c) {
 	return $c->status(403) unless $c->is_global_admin;
-	return $c->status(200, Conch::Model::DatacenterRoom->all());
+
+	my $r = Conch::Model::DatacenterRoom->all();
+	$c->log->debug("Found ".scalar($r->@*)." datacenter rooms");
+
+	return $c->status(200 => $r);
 }
 
 
@@ -69,9 +80,13 @@ Create a new datacenter room
 sub create ($c) {
 	return $c->status(403) unless $c->is_global_admin;
 	my $i = $c->validate_input('DatacenterRoomCreate');
-	return if not $i;
+	if(not $i) {
+		$c->log->warn("Input failed validation");
+		return;
+	}
 
 	my $r = Conch::Model::DatacenterRoom->new($i->%*)->save;
+	$c->log->debug("Created datacenter room ".$r->id);
 	$c->status(303 => "/room/".$r->id);
 }
 
@@ -85,9 +100,13 @@ Update an existing room
 sub update ($c) {
 	return $c->status(403) unless $c->is_global_admin;
 	my $i = $c->validate_input('DatacenterRoomUpdate');
-	return if not $i;
+	if(not $i) {
+		$c->log->warn("Input failed validation");
+		return;
+	}
 
 	$c->stash('datacenter_room')->update($i->%*)->save();
+	$c->log->debug("Updated datacenter room ".$c->stash('datacenter_room')->id);
 	$c->status(303 => "/room/".$c->stash('datacenter_room')->id);
 }
 
@@ -101,6 +120,7 @@ Permanently delete a datacenter room
 sub delete ($c) {
 	return $c->status(403) unless $c->is_global_admin;
 	$c->stash('datacenter_room')->burn;
+	$c->log->debug("Deleted datacenter room ".$c->stash('datacenter_room')->id);
 	return $c->status(204);
 }
 
@@ -112,11 +132,14 @@ sub delete ($c) {
 sub racks ($c) {
 	return $c->status(403) unless $c->is_global_admin;
 
-	return $c->status(
-		200 => Conch::Model::DatacenterRack->from_datacenter_room(
-			$c->stash('datacenter_room')->id
-		)
+	my $r = Conch::Model::DatacenterRack->from_datacenter_room(
+		$c->stash('datacenter_room')->id
 	);
+	$c->log->debug(
+		"Found ".scalar($r->@*).
+		" racks for datacenter room ".$c->stash('datacenter_room')->id
+	);
+	return $c->status(200 => $r);
 
 }
 

lib/Conch/Controller/Device.pm
@@ -10,10 +10,13 @@ Conch::Controller::Device
 
 package Conch::Controller::Device;
 
+use Role::Tiny::With;
 use Mojo::Base 'Mojolicious::Controller', -signatures;
 use Conch::UUID 'is_uuid';
 
 use aliased 'Conch::Class::DeviceDetailed';
+
+with 'Conch::Role::MojoLog';
 
 use Conch::Models;
 
@@ -27,14 +30,19 @@ endpoint
 
 sub under ($c) {
 	my $device_id = $c->param('id');
-	my $device =
-		Conch::Model::Device->lookup_for_user( $c->stash('user_id'),
-		$device_id, );
+	$c->log->debug("Looking up device $device_id for user ".$c->stash('user_id'));
+
+	my $device = Conch::Model::Device->lookup_for_user(
+		$c->stash('user_id'),
+		$device_id,
+	);
+
 	if ($device) {
+		$c->log->debug("Found device ".$device->id);
 		$c->stash( current_device => $device );
 		return 1;
-	}
-	else {
+	} else {
+		$c->log->debug("Failed to find device $device_id");
 		$c->status( 404, { error => "Device '$device_id' not found" } );
 		return 0;
 	}
@@ -85,10 +93,17 @@ Sets the C<graduated> field on a device, unless that field has already been set
 sub graduate($c) {
 	my $device    = $c->stash('current_device');
 	my $device_id = $device->id;
-	return $c->status( 409, "Device $device_id has already been graduated" )
-		if defined( $device->graduated );
+
+	# FIXME this shouldn't be an error
+	if(defined($device->graduated)) {
+		$c->log->debug("Device $device_id has already been graduated");
+		return $c->status( 409 => {
+			error => "Device $device_id has already been graduated"
+		})
+	}
 
 	$device->graduate;
+	$c->log->debug("Marked $device_id as graduated");
 
 	$c->status(303);
 	$c->redirect_to( $c->url_for("/device/$device_id")->to_abs );
@@ -104,6 +119,8 @@ sub set_triton_reboot ($c) {
 	my $device = $c->stash('current_device');
 	$device->set_triton_reboot;
 
+	$c->log->debug("Marked ".$device->id." as rebooted into triton");
+
 	$c->status(303);
 	$c->redirect_to( $c->url_for( '/device/' . $device->id )->to_abs );
 }
@@ -118,15 +135,16 @@ valid UUID
 sub set_triton_uuid ($c) {
 	my $device = $c->stash('current_device');
 	my $triton_uuid = $c->req->json && $c->req->json->{triton_uuid};
-	return $c->status(
-		400,
-		{
-			error =>
-				"'triton_uuid' attribute must be present in JSON object and a UUID"
-		}
-	) unless defined($triton_uuid) && is_uuid($triton_uuid);
+
+	unless(defined($triton_uuid) && is_uuid($triton_uuid)) {
+		$c->log->warn("Input failed validation"); # FIXME use the validator
+		return $c->status(400 => {
+			error => "'triton_uuid' attribute must be present in JSON object and a UUID"
+		});
+	}
 
 	$device->set_triton_uuid($triton_uuid);
+	$c->log->debug("Set the triton uuid for device ".$device->id." to $triton_uuid");
 
 	$c->status(303);
 	$c->redirect_to( $c->url_for( '/device/' . $device->id )->to_abs );
@@ -142,22 +160,27 @@ the C<triton_setup> field. Fails if the device has already been marked as such.
 sub set_triton_setup ($c) {
 	my $device    = $c->stash('current_device');
 	my $device_id = $device->id;
-	return $c->status(
-		409,
-		{
-			error =>
-"Device $device_id must be marked as rebooted into Triton and the Trition "
-				. "UUID set before it can be marked as set up for Triton"
-		}
-		)
-		unless ( defined( $device->latest_triton_reboot )
-		&& defined( $device->triton_uuid ) );
 
-	return $c->status( 409,
-		"Device $device_id has already been marked as set up for Triton" )
-		if defined( $device->triton_setup );
+	unless ( defined( $device->latest_triton_reboot )
+		&& defined( $device->triton_uuid ) ) {
+
+		$c->log->warn("Input failed validation");
+
+		return $c->status(409 => {
+			error => "Device $device_id must be marked as rebooted into Triton and the Trition UUID set before it can be marked as set up for Triton"
+		});
+	}
+
+	# FIXME this should not be an error
+	if (defined($device->triton_setup)) {
+		$c->log->debug("Device $device_id has already been marked as set up for Triton");
+		return $c->status( 409 => {
+			error => "Device $device_id has already been marked as set up for Triton"
+		})
+	}
 
 	$device->set_triton_setup;
+	$c->log->debug("Device $device_id marked as set up for triton");
 
 	$c->status(303);
 	$c->redirect_to( $c->url_for("/device/$device_id")->to_abs );
@@ -172,15 +195,16 @@ Sets the C<asset_tag> field on a device
 sub set_asset_tag ($c) {
 	my $device = $c->stash('current_device');
 	my $asset_tag = $c->req->json && $c->req->json->{asset_tag};
-	return $c->status(
-		400,
-		{
-			error =>
-"'asset_tag' attribute must be present and in JSON object a string value"
-		}
-	) unless defined($asset_tag) && ref($asset_tag) eq '';
+
+	unless(defined($asset_tag) && ref($asset_tag) eq '') {
+		$c->log->warn("Input failed validation"); #FIXME use the validator
+		return $c->status(400 => {
+			error => "'asset_tag' attribute must be present and in JSON object a string value"
+		});
+	}
 
 	$device->set_asset_tag($asset_tag);
+	$c->log->debug("Set the asset tag for device ".$device->id." to $asset_tag");
 
 	$c->status(303);
 	$c->redirect_to( $c->url_for( '/device/' . $device->id )->to_abs );
@@ -198,6 +222,7 @@ sub set_validated($c) {
 	return $c->status(204) if defined( $device->validated );
 
 	$device->set_validated();
+	$c->log->debug("Marked the device $device_id as validated");
 
 	$c->status(303);
 	$c->redirect_to( $c->url_for("/device/$device_id")->to_abs );

commit 3c88938edeae2bc7d67c1a55aea125f7335d12d1

add_user_to_workspace takes an id, not an object
issue introduced in commit bb2c8e26c
fixes rollbar exception #43
This change is not relevant to master given PR#337.

lib/Conch/Controller/WorkspaceUser.pm
@@ -96,7 +96,7 @@ sub invite ($c) {
 
 	Conch::Model::Workspace->new->add_user_to_workspace(
 		$user->id,
-		$c->stash('current_workspace'),
+		$c->stash('current_workspace')->id,
 		$maybe_role->id
 	);
 	$c->log->info("Add user ".$user->id." to workspace ".$c->stash('current_workspace')->id);