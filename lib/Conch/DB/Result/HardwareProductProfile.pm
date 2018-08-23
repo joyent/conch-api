ef3613f0f87126b95412dafd3c461d8b4b313f25
Karen Etheridge <ether@joyent.com>
2018-08-23 12:39:30 -0700

    fix these relationship names
    device_criteria really ought to be named "device_validate_criterion", but... ugh.

--- lib/Conch/DB/Result/HardwareProductProfile.pm
+++ lib/Conch/DB/Result/HardwareProductProfile.pm
@@ -295,7 +295,7 @@ __PACKAGE__->has_many(
   { cascade_copy => 0, cascade_delete => 0 },
 );
 
-=head2 device_validate_criterias
+=head2 device_validate_criteria
 
 Type: has_many
 
@@ -304,7 +304,7 @@ Related object: L<Conch::DB::Result::DeviceValidateCriteria>
 =cut
 
 __PACKAGE__->has_many(
-  "device_validate_criterias",
+  "device_validate_criteria",
   "Conch::DB::Result::DeviceValidateCriteria",
   { "foreign.hardware_product_id" => "self.id" },
   { cascade_copy => 0, cascade_delete => 0 },
@@ -361,8 +361,8 @@ __PACKAGE__->belongs_to(
 );
 
 
-# Created by DBIx::Class::Schema::Loader v0.07049 @ 2018-08-23 14:04:56
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:89+BBiaTeiAf9hqD55Gofw
+# Created by DBIx::Class::Schema::Loader v0.07049 @ 2018-08-23 14:06:26
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:aJNvEedlKdVa2F9Nn3VXRg
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
