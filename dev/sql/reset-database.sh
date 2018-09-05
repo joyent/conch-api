cb2fa0dafc4482f2b2bbed4d2aa8ebbadc83855d
Karen Etheridge <ether@joyent.com>
2018-09-05 11:42:19 -0700

    fix db reset script

--- dev/sql/reset-database.sh
+++ dev/sql/reset-database.sh
@@ -2,11 +2,11 @@
 
 BASEDIR=$(cd `dirname $0` && pwd)
 
-psql -d postgres -c 'DROP DATABASE conch'
-psql -d postgres -c 'DROP DATABASE conch'
-psql -d postgres -c 'CREATE ROLE conch LOGIN'
-psql -d postgres -c 'CREATE DATABASE conch OWNER conch'
-psql -U postgres -d conch -c 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";'
-psql -U postgres -d conch -c 'CREATE EXTENSION IF NOT EXISTS "pgcrypto";'
+sudo -u postgres psql -d postgres -c 'DROP DATABASE conch'
+sudo -u postgres psql -d postgres -c 'DROP USER conch'
+sudo -u postgres psql -d postgres -c 'CREATE ROLE conch LOGIN'
+sudo -u postgres psql -d postgres -c 'CREATE DATABASE conch OWNER conch'
+sudo -u postgres psql -U postgres -d conch -c 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";'
+sudo -u postgres psql -U postgres -d conch -c 'CREATE EXTENSION IF NOT EXISTS "pgcrypto";'
 
-$BASEDIR/../sql/run_migrations.sh
+$BASEDIR/../../sql/run_migrations.sh
