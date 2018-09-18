71eb0e1469c9cbfbdc3a9d4ca855ec898646f833
Karen Etheridge <ether@joyent.com>
2018-09-18 14:08:17 -0700

    extend workspace queries to search recursively up and down the heirarchy
    These form the building blocks of all workspace queries in the
    application, to ensure that user permissions cascade from parent
    workspaces to children.

--- lib/Conch/DB/ResultSet/Workspace.pm
+++ lib/Conch/DB/ResultSet/Workspace.pm
@@ -3,6 +3,8 @@ use v5.26;
 use warnings;
 use parent 'Conch::DB::ResultSet';
 
+use Conch::UUID 'is_uuid';
+
 =head1 NAME
 
 Conch::DB::ResultSet::Workspace
@@ -10,6 +12,12 @@ Conch::DB::ResultSet::Workspace
 =head1 DESCRIPTION
 
 Interface to queries involving workspaces.
+
+Note: in the methods below, "above" and "beneath" are referring to the workspace tree,
+where the root ("GLOBAL") workspace is considered to be at the top and child
+workspaces hang below as nodes and leaves.
+
+A parent workspace is "above" a given workspace; its children are "beneath".
 
 =head1 METHODS
 
@@ -23,12 +31,13 @@ sub workspaces_beneath {
     my ($self, $workspace_id) = @_;
 
     Carp::croak('missing workspace_id') if not defined $workspace_id;
+    Carp::croak('resultset should not have conditions') if $self->{attrs}{cond};
 
     my $query = q{
 WITH RECURSIVE workspace_children (id) AS (
   SELECT id
-    FROM workspace
-    WHERE parent_workspace_id = ?
+    FROM workspace base
+    WHERE base.parent_workspace_id = ?
   UNION
     SELECT child.id
     FROM workspace child, workspace_children parent
@@ -38,6 +47,108 @@ SELECT workspace_children.id FROM workspace_children
 };
 
     $self->search({ $self->current_source_alias . '.id' => { -in => \[ $query, $workspace_id ] } });
+}
+
+=head2 and_workspaces_beneath
+
+As L<workspaces_beneath>, but also includes the original workspace.
+
+C<$workspace_id> can be a single workspace_id, an arrayref of multiple ids, or a subquery (via
+C<< $resultset->as_query >>, which must return a single column of workspace_id(s)).
+
+=cut
+
+sub and_workspaces_beneath {
+    my ($self, $workspace_id) = @_;
+
+    Carp::croak('missing workspace_id') if not defined $workspace_id;
+    Carp::croak('resultset should not have conditions') if $self->{attrs}{cond};
+
+    my ($workspace_id_clause, @binds) = $self->_workspaces_subquery($workspace_id);
+
+    my $query = qq{
+WITH RECURSIVE workspace_and_children (id) AS (
+  SELECT id
+    FROM workspace base
+    WHERE (base.id $workspace_id_clause)
+  UNION
+    SELECT child.id
+    FROM workspace child, workspace_and_children parent
+    WHERE child.parent_workspace_id = parent.id
+)
+SELECT DISTINCT workspace_and_children.id FROM workspace_and_children
+};
+
+    $self->search({ $self->current_source_alias . '.id' => { -in => \[ $query, @binds ] } });
+}
+
+=head2 workspaces_above
+
+Chainable resultset that finds all workspaces above the provided workspace id (that is, all
+parent workspaces, up to the root).
+The resultset does *not* include the original workspace itself -- see
+L</and_workspaces_above> for that.
+
+=cut
+
+sub workspaces_above {
+    my ($self, $workspace_id) = @_;
+
+    Carp::croak('missing workspace_id') if not defined $workspace_id;
+    Carp::croak('resultset should not have conditions') if $self->{attrs}{cond};
+
+    my $query = qq{
+WITH RECURSIVE workspace_parents (id, parent_workspace_id) AS (
+  SELECT base.id, base.parent_workspace_id
+    FROM workspace base
+    JOIN workspace base_child ON base_child.parent_workspace_id = base.id
+    WHERE base_child.id = ?
+  UNION
+    SELECT parent.id, parent.parent_workspace_id
+    FROM workspace parent, workspace_parents child
+    WHERE parent.id = child.parent_workspace_id
+)
+SELECT workspace_parents.id FROM workspace_parents
+};
+
+    $self->search({ $self->current_source_alias . '.id' => { -in => \[ $query, $workspace_id ] } });
+}
+
+=head2 and_workspaces_above
+
+Chainable resultset that finds all workspaces above the provided workspace id (that is, all
+parent workspaces, up to the root).
+
+The resultset includes the original workspace itself.
+
+C<$workspace_id> can be a single workspace_id, an arrayref of multiple ids, or a subquery (via
+C<< $resultset->as_query >>, which must return a single column of workspace_id(s)).
+
+=cut
+
+sub and_workspaces_above {
+    my ($self, $workspace_id) = @_;
+
+    # move to subfunction if carp_not doesn't include @ISA
+    Carp::croak('missing workspace_id') if not defined $workspace_id;
+    Carp::croak('resultset should not have conditions') if $self->{attrs}{cond};
+
+    my ($workspace_id_clause, @binds) = $self->_workspaces_subquery($workspace_id);
+
+    my $query = qq{
+WITH RECURSIVE workspace_and_parents (id, parent_workspace_id) AS (
+  SELECT id, parent_workspace_id
+    FROM workspace base
+    WHERE (base.id $workspace_id_clause)
+  UNION
+    SELECT parent.id, parent.parent_workspace_id
+    FROM workspace parent, workspace_and_parents child
+    WHERE parent.id = child.parent_workspace_id
+)
+SELECT DISTINCT workspace_and_parents.id FROM workspace_and_parents
+};
+
+    $self->search({ $self->current_source_alias . '.id' => { -in => \[ $query, @binds ] } });
 }
 
 =head2 associated_racks
@@ -68,6 +179,40 @@ sub associated_racks {
         },
         { alias => 'datacenter_rack' },
     );
+}
+
+=head2 _workspaces_subquery
+
+Generate values for inserting into a recursive query.
+The first value is a string to be added after C<< WHERE <column> >>; the remainder are bind
+values to be used in C<< \[ $query_string, @binds ] >>.
+
+C<$workspace_id> can be a single workspace_id, an arrayref of multiple ids, or a subquery (via
+C<< $resultset->as_query >>, which must return a single column of workspace_id(s)).
+
+=cut
+
+sub _workspaces_subquery {
+    my ($self, $workspace_id) = @_;
+
+    if (not ref $workspace_id and is_uuid($workspace_id)) {
+        return ('= ?', $workspace_id);
+    }
+
+    if (ref $workspace_id eq 'ARRAY') {
+        return ('= ANY(?)', $workspace_id);
+    }
+
+    if (ref $workspace_id eq 'REF' and ref $workspace_id->$* eq 'ARRAY') {
+        return (
+            'IN ' . $workspace_id->$*->[0],
+            $workspace_id->$*->@[1 .. $workspace_id->$*->$#*],
+        );
+    }
+
+    require Data::Dumper;
+    Carp::croak('I don\'t know what to do with workspace_id argument ',
+        Data::Dumper->new([ $workspace_id ])->Indent(0)->Terse(1)->Dump);
 }
 
 1;
