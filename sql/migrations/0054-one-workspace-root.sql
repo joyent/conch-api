65cf5cf39ed626482548259f7ac8c0bc539f4c5b
Karen Etheridge <ether@joyent.com>
2018-09-20 16:27:17 -0700

    add a constraint so we can only ever have one parentless workspace

---
+++ sql/migrations/0054-one-workspace-root.sql
@@ -0,0 +1,7 @@
+SELECT run_migration(54, $$
+
+    -- thanks to https://www.dbrnd.com/2017/12/postgresql-allow-single-null-for-unique-constraint-column/
+    create unique index workspace_parent_id_idx on workspace
+        ((parent_workspace_id is null)) where parent_workspace_id is null;
+
+$$);
