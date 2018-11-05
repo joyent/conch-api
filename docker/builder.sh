7ebbb002338d6a2e7ea3f2ecf2f098d0d9f4a2ef
sungo <sungo@sungo.us>
2018-11-05 11:02:32 -0500

    in the docker builder, always burn the intermediate images, even in the face of failure

--- docker/builder.sh
+++ docker/builder.sh
@@ -9,6 +9,7 @@ HASH=`git rev-parse HEAD`
 LABEL=$(echo "${LABEL}" | sed 's/\//_/g')
 
 docker build \
+	--force-rm \
 	-t ${PREFIX}/conch-api:${LABEL} \
 	--build-arg VERSION=${TAG} \
 	--build-arg VCS_REF=${HASH} \
