fe823a9f8e2c1399d310384942dbde2f570543d5
Dustin Ryerson <dpryerson@gmail.com>
2019-03-25 15:06:27 -0700

    Fixes typo in log

--- lib/Conch/Controller/User.pm
+++ lib/Conch/Controller/User.pm
@@ -236,7 +236,7 @@ sub reset_user_password ($c) {
 	);
 
 	if ($c->req->query_params->param('clear_tokens') // 1) {
-		$c->log->warn('user ' . $c->stash('user')->name . ' deleting user session tokens for for user ' . $user->name);
+		$c->log->warn('user ' . $c->stash('user')->name . ' deleting user session tokens for user ' . $user->name);
 		$user->delete_related('user_session_tokens');
 
 		%update = (
@@ -437,7 +437,7 @@ sub deactivate ($c) {
 	$user->delete_related('user_workspace_roles');
 
 	if ($c->req->query_params->param('clear_tokens') // 1) {
-		$c->log->warn('user ' . $c->stash('user')->name . ' deleting user session tokens for for user ' . $user->name);
+		$c->log->warn('user ' . $c->stash('user')->name . ' deleting user session tokens for user ' . $user->name);
 		$user->delete_related('user_session_tokens');
 	}
 
