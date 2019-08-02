4a2cafaa549dba8f2fadd787ed08126e4a2ebefc
Karen Etheridge <ether@joyent.com>
2019-08-02 13:04:47 -0700

    speed up validation plan runs by adding an appropriate index
    ..which is used to determine if a suitable validation_result record already
    exists, before creating a new one
    
    (schema update cherry-picked from commit 40852cb4b054958b523171154acc259bf5e97e54
    which was in v2.32.5, and then adjusted for v3)

--- sql/migrations/0100-assert-safe-for-v3.sql
+++ sql/migrations/0100-assert-safe-for-v3.sql
@@ -1,5 +1,5 @@
 do $$ begin
-assert (select max(id) from migration) = 91, 'not all v2 migrations have been run; cannot proceed with v3 upgrade';
+assert (select max(id) from migration) = 92, 'not all v2 migrations have been run; cannot proceed with v3 upgrade';
 end; $$;
 
 SELECT run_migration(100, '');
