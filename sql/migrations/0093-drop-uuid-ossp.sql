29f158eb12f56a1bd12214f81132acfef34d376a
Karen Etheridge <ether@joyent.com>
2019-08-09 09:00:09 -0700

    back out of the dropping of the postgres extension
    ..as this failed when applying it on staging.
    
    (this reverts part of commit 5c9ebcf5a33b5ba3df37cbd32b6e92fc586d3a5b)

--- sql/migrations/0093-drop-uuid-ossp.sql
+++ sql/migrations/0093-drop-uuid-ossp.sql
@@ -6,6 +6,8 @@ SELECT run_migration(94, $$
     alter table validation_state alter column id set default gen_random_uuid();
     alter table workspace alter column id set default gen_random_uuid();
 
-    drop extension "uuid-ossp";
+    -- sadly, we can't do this in a migration:
+    -- "ERROR:  must be owner of extension uuid-ossp"
+    -- drop extension "uuid-ossp";
 
 $$);
--- sql/schema.sql
+++ sql/schema.sql
@@ -44,6 +44,20 @@ COMMENT ON EXTENSION pgcrypto IS 'cryptographic functions';
 
 
 --
+-- Name: uuid-ossp; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION "uuid-ossp"; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION "uuid-ossp" IS 'generate universally unique identifiers (UUIDs)';
+
+
+--
 -- Name: device_health_enum; Type: TYPE; Schema: public; Owner: conch
 --
 
