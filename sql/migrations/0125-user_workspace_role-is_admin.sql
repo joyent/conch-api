73aa8165d840fafe762a33b4d7174157367459eb
Karen Etheridge <ether@joyent.com>
2019-08-14 09:10:13 -0700

    fix this migration to only delete rows for sysadmin users

--- sql/migrations/0125-user_workspace_role-is_admin.sql
+++ sql/migrations/0125-user_workspace_role-is_admin.sql
@@ -2,6 +2,8 @@ SELECT run_migration(125, $$
 
     delete from user_workspace_role
         using user_account
-        where user_account.is_admin is true;
+        where
+            user_account.id = user_workspace_role.user_id
+            and user_account.is_admin is true;
 
 $$);
