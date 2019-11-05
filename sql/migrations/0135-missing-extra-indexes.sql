642f70ab5f53ffd38b1a82add534cd38210d425c
Karen Etheridge <ether@joyent.com>
2019-11-05 09:16:45 -0800

    some of these constraints and indexes may already be removed in v2
    see migration 0094, #937 in v2.37.2.

--- sql/migrations/0135-missing-extra-indexes.sql
+++ sql/migrations/0135-missing-extra-indexes.sql
@@ -13,12 +13,12 @@ SELECT run_migration(135, $$
     -- Postgres will use the multi-column index for queries on the first column."
     -- https://www.monkeyatlarge.com/archives/2011/02/08/redundant-indexing-in-postgresql/
 
-    drop index device_relay_connection_device_id_idx;   -- see device_relay_connection_pkey
-    drop index device_report_device_id_idx;             -- see device_report_device_id_created_idx
-    drop index user_relay_connection_user_id_idx;       -- see user_relay_connection_pkey
-    drop index user_workspace_role_user_id_idx;         -- see user_workspace_role_pkey
-    drop index validation_state_device_id_idx;          -- see validation_state_device_id_validation_plan_id_completed_idx
-    drop index validation_state_member_validation_state_id_idx; -- see validation_state_member_pkey
-    drop index workspace_rack_workspace_id_idx;         -- see workspace_rack_pkey
+    drop index if exists device_relay_connection_device_id_idx;   -- see device_relay_connection_pkey
+    drop index if exists device_report_device_id_idx;             -- see device_report_device_id_created_idx
+    drop index if exists user_relay_connection_user_id_idx;       -- see user_relay_connection_pkey
+    drop index if exists user_workspace_role_user_id_idx;         -- see user_workspace_role_pkey
+    drop index if exists validation_state_device_id_idx;          -- see validation_state_device_id_validation_plan_id_completed_idx
+    drop index if exists validation_state_member_validation_state_id_idx; -- see validation_state_member_pkey
+    drop index if exists workspace_rack_workspace_id_idx;         -- see workspace_rack_pkey
 
 $$);
