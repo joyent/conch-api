eecfcb88bb348b3a877153096f01ffae35837f63
Karen Etheridge <ether@joyent.com>
2020-10-05 12:40:55 -0700

    fix validation_state_member.result_order uniquification
    Migration 0147 blows up because result_order is not yet unique across all results for a validation_state.
    Move this migration to run later, and add another migration before it that fixes the duplication.

--- sql/migrations/0150-validation_state_member-all-columns-unique.sql
+++ sql/migrations/0150-validation_state_member-all-columns-unique.sql
@@ -1,4 +1,4 @@
-SELECT run_migration(147, $$
+SELECT run_migration(150, $$
 
     alter table validation_state_member
         add constraint validation_state_member_validation_state_id_result_order_key unique (validation_state_id, result_order);
